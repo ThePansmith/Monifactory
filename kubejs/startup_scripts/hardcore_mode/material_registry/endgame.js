109c5996b3dabe6ff59ffb15af35e1b3f0e6b1d4
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-05-09 12:33:02 -0500

    HM work
    Rock and water gen now have correct recipes
    Kapton K chain

--- kubejs/startup_scripts/hardcore_mode/material_registry/endgame.js
+++ kubejs/startup_scripts/hardcore_mode/material_registry/endgame.js
@@ -1,16 +1,17 @@
 //? Keep in sync with
 //? https://github.com/Nomi-CEu/Nomi-Labs/blob/main/src/main/java/com/nomiceu/nomilabs/gregtech/material/registry/register/LabsEndgame.java
 
+// Kapton K disabled in favor of GCYr's chain
 GTCEuStartupEvents.registry('gtceu:material', event => {
     if (!isExpertMode) {
         return;
     }
 
-    event.create("kapton_k")
-        .ingot().fluid()
-        .color(0xffce52).iconSet('dull')
-        .flags(GTMaterialFlags.NO_SMELTING, GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.DISABLE_DECOMPOSITION)
-        .components('22x carbon', '10x hydrogen', '2x nitrogen', '5x oxygen')
+    // event.create("kapton_k")
+    //     .ingot().fluid()
+    //     .color(0xffce52).iconSet('dull')
+    //     .flags(GTMaterialFlags.NO_SMELTING, GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.DISABLE_DECOMPOSITION)
+    //     .components('22x carbon', '10x hydrogen', '2x nitrogen', '5x oxygen')
 
 })
 
@@ -18,5 +19,5 @@ GTCEuStartupEvents.materialModification(() => {
     if (!isExpertMode) {
         return;
     }
-	GTMaterials.get('kapton_k').setFormula('C6H2((CO)2N)2C6H4OC6H4');
+	// GTMaterials.get('kapton_k').setFormula('C6H2((CO)2N)2C6H4OC6H4');
 })
