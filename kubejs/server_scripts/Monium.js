3b9756569cfa442bdc12172129ef28dd19242df9
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-05-20 12:26:04 -0500

    Update Monium.js

--- kubejs/server_scripts/Monium.js
+++ kubejs/server_scripts/Monium.js
@@ -1,9 +1,9 @@
 ServerEvents.recipes(event => {
 
         //quField_Stabilised_Omnic compound
-        event.recipes.gtceu.omnic_forge('fieldstabilizedCompound')
-        .itemInputs('5x gtceu:uiv_field_generator', '12x gtceu:omnic_acid_dust', '3x gtceu:uhv_field_generator', '4x gtceu:uev_field_generator', '10x kubejs:quasi_stable_neutron_star')      
-        .itemOutputs('kubejs:field_stabilised_omnic_pulsar_compound')       
+        event.recipes.gtceu.omnic_forge('fieldstabilizedcompound')
+        .itemInputs('5x gtceu:uiv_field_generator', '12x gtceu:omnic_acid_dust', '3x gtceu:uhv_field_generator', '4x gtceu:uev_field_generator', '10x kubejs:quasi_stable_neutron_star')
+        .itemOutputs('kubejs:field_stabilised_omnic_pulsar_compound')
         .duration(255)
         .EUt(16380)
 
@@ -15,9 +15,8 @@ ServerEvents.recipes(event => {
         .duration(50)
 
         event.recipes.gtceu.omnic_forge('singularity_containment_unit')
-        .itemInputs('8x gtceu:monium_plate', '4x gtceu:neutron_reflector', '2x gtceu:uiv_sensor', '2x gtceu:uev_sensor', 'gtceu:uiv_field_generator')      
-        .itemOutputs('64x kubejs:singularity_containment_unit')       
+        .itemInputs('8x gtceu:monium_plate', '4x gtceu:neutron_reflector', '2x gtceu:uiv_sensor', '2x gtceu:uev_sensor', 'gtceu:uiv_field_generator')
+        .itemOutputs('64x kubejs:singularity_containment_unit')
         .duration(300)
         .EUt(16380)
-        
 })
