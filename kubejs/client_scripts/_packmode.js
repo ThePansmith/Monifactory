e126866b659b04ecb514c1da2c751aae85decf96
CubeBotFan <150861277+cubebotfan@users.noreply.github.com>
2024-05-23 15:14:42 -0500

    simplify packmode scripts

--- kubejs/client_scripts/_packmode.js
+++ kubejs/client_scripts/_packmode.js
@@ -1,28 +1,5 @@
 //priority: 1000
 
-const defaultConfig = {
-    mode: 'normal',
-    message: 'This tells KubeJS what mode the pack is currently in, packmode is changed in pack-mode-switcher'
-};
-const configName = 'config/packmode.json';
-let config = JsonIO.read(configName);
-if (!config || !config.mode) {
-    JsonIO.write(configName, defaultConfig);
-    console.log(`Created new ${configName}`);
-    config = defaultConfig;
-}
-if (config.mode == 'none') {
-    JsonIO.write(configName, defaultConfig);
-    config.mode = defaultConfig.mode;
-    console.log(`Overwrote ${configName}, because the mode 'none' was found. Valid modes are 'normal', 'hard' and 'harder'.`);
-}
-
-let packMode = config.mode;
-
-global.packmode = packMode;
-global.isNormalMode = packMode == 'normal';
-global.isHardMode = packMode == 'hard';
-global.isHarderMode = packMode == 'harder';
-const isNormalMode = packMode == 'normal';
-const isHardMode = packMode == 'hard';
-const isHarderMode = packMode == 'harder';
+const isNormalMode = global.isNormalMode
+const isHardMode = global.isHardMode
+const isHarderMode = global.isHarderMode
