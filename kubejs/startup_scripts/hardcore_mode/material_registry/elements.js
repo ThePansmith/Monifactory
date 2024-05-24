b0bf1944cff0f0880a629abc1616471e00b5ba84
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-05-24 08:42:21 +0100

    Bring some stuff upto speed with #245

--- kubejs/startup_scripts/hardcore_mode/material_registry/elements.js
+++ kubejs/startup_scripts/hardcore_mode/material_registry/elements.js
@@ -16,8 +16,4 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
         .color(0xff00ff).iconSet('bright')
         .flags(GTMaterialFlags.GENERATE_PLATE, GTMaterialFlags.GENERATE_DENSE)
         .blastTemp(10800, 'highest')
-
-    GTMaterials.Iridium.addFlags(GTMaterialFlags.GENERATE_DENSE)
-    GTMaterials.Berkelium.setProperty(PropertyKey.INGOT, new $IngotProperty());
-    GTMaterials.Berkelium.addFlags(GTMaterialFlags.GENERATE_FRAME)
 })
