6c39af03a6f2c3d2d2103aeb45ca8b6abcdafd5c
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-05-24 11:50:16 +0100

    Bump basic projector from 16 to 20 slots

--- kubejs/startup_scripts/Multiblock_Registry.js
+++ kubejs/startup_scripts/Multiblock_Registry.js
@@ -64,7 +64,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('basic_microverse')
         .category('multiblock')
         .setEUIO('in')
-        .setMaxIOSize(4, 16, 1, 0)
+        .setMaxIOSize(4, 20, 1, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.COOLING);
