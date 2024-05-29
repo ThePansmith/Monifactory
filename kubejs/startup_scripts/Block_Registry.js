fd87dee6488a46a09b2faf37cea598b1382eac79
SparkedTheorem <162088357+SparkedTheorem@users.noreply.github.com>
2024-05-29 07:01:48 -0700

    Supercomputer Circuit Changes, more Atomic Reconstructors and more (#270)
    * Update circuits.js
    
    -brought in modified recipes from Nomi CEu, Nano Supercomputers now use Lumium and Tungstensteel Fine Wires, and Crystal Supercomputers use Enderium in place of Niobium Titanium wires
    
    * Update greenhouse.js
    
    -Added all Flowers + Vines + Lily Pads
    
    * Extra Atomic Reconstructor Tiers
    
    -Added new tiers to Atomic Reconstructor (EV, LuV, UV, and UEV)
    -also fixed lang stuff for Rock Cycle Simulator
    
    * sculk compound casing textures
    
    * Update Multiblock_Registry.js
    
    update Sculk Reverbator to use new textures
    
    * Update sculk_compound_casing.png
    
    * almost forgot to update block registry ooopppssssss
    
    * Also added Resonant Fusion Coil and Casing :trollface:

--- kubejs/startup_scripts/Block_Registry.js
+++ kubejs/startup_scripts/Block_Registry.js
@@ -126,7 +126,7 @@ StartupEvents.registry("block", event => {
         .hardness(5)
         .tagBlock("mineable/pickaxe")
         .requiresTool(true)
-        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/casing')})
+        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/sculk_compound_casing')})
 
     event.create('resonant_sculk_compound_casing', 'gtceu:renderer')
         .displayName("Resonant Sculk Compound Casing")
@@ -144,7 +144,7 @@ StartupEvents.registry("block", event => {
         .hardness(5)
         .tagBlock("mineable/pickaxe")
         .requiresTool(true)
-        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/fusion')})
+        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/resonant_fusion_casing')})
 
     event.create('resonant_fusion_coil', 'gtceu:renderer')
         .displayName("Resonant Fusion Coil")
@@ -153,7 +153,7 @@ StartupEvents.registry("block", event => {
         .hardness(5)
         .tagBlock("mineable/pickaxe")
         .requiresTool(true)
-        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/coil')})
+        .textureOverrideRenderer('minecraft:block/cube_all', {'all': new ResourceLocation('kubejs', 'block/sculk_compound/resonant_fusion_coil')})
 
         event.create("dark_soularium_casing", 'gtceu:renderer')
         .displayName("Dark Soularium Casing")
