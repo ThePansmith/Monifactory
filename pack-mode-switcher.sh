af5518b8e3739d22c48c2428a708f38869f5f962
Lynxen <98620121+TheRealLynx@users.noreply.github.com>
2024-05-29 16:54:31 +0200

    Fixed pack-mode-switcher.sh so that it works (#269)
    * Update pack-mode-switcher.sh
    
    * added support for harder mode in the pack mode switcher
    
    ---------
    
    Co-authored-by: Lynxen <Vidar>

--- pack-mode-switcher.sh
+++ pack-mode-switcher.sh
@@ -12,14 +12,15 @@ touch .mode
 set -e
 printf "${POWDER_BLUE}Monifactory | Pack Mode Switcher${NORMAL}"
 
-NORMAL_CFG=config-overrides/normal
-HARDMODE_CFG=config-overrides/hardmode
-TARGET=./config
+NORMAL_CFG='./config-overrides/normal'
+HARDMODE_CFG='./config-overrides/hardmode'
+HARDER_CFG='./config-overrides/harder'
+TARGET='./config'
 CURRENT_MODE="$(head .mode)"
 CURRENT_MODE=${CURRENT_MODE:="normal"}
 
 # Check if config-overrides dir exists
-if [[ ! -d "${NORMAL_CFG}" ]] || [[ ! -d "${EXPERT_CFG}" ]]; then
+if [[ ! -d "${NORMAL_CFG}" ]] || [[ ! -d "${HARDMODE_CFG}" ]] || [[ ! -d "${HARDER_CFG}" ]] ; then
   printf "\n\n${RED}Could not find \`config-overrides\` directory! \nMake sure you are in the \`/minecraft\` directory of your instance! (The one containing \`/config\`)${NORMAL}\n"
   printf "${YELLOW}Otherwise, if you are in the \`/minecraft\` directory, please try reinstalling the pack.${NORMAL}\n"
   exit 1
@@ -31,7 +32,7 @@ fi
 printf "\n\n${YELLOW}Current Mode: ${CURRENT_MODE}${NORMAL}\n"
 
 if [ -z "$1" ]; then
-  printf "${POWDER_BLUE}Set Pack Mode: [Normal / Hardmode]: ${NORMAL}"
+  printf "${POWDER_BLUE}Set Pack Mode: [Normal / Hardmode / Harder]: ${NORMAL}"
   read MODE
 else
   MODE="$1"
@@ -67,9 +68,23 @@ case $MODE in
     echo hard > .mode
   ;;
 
+  E|e|harder|Harder)
+
+    cp -rf "$HARDER_CFG/." ${TARGET}
+
+    if [ -f "server.properties" ]; then
+        mv "${TARGET}/server.properties" ./
+    else
+        rm "${TARGET}/server.properties" || true
+    fi
+
+    # Update Mode
+    echo harder > .mode
+  ;;
+
   *)
     printf "\n${RED}Error: Invalid input ${MODE}!${NORMAL}\n"
-    printf "\n${POWDER_BLUE}Accepted Inputs:\n${YELLOW}- [Normal, normal, N, n]\n- [Hardmode, hardmode, H, h]${NORMAL}\n"
+    printf "\n${POWDER_BLUE}Accepted Inputs:\n${YELLOW}- [Normal, normal, N, n]\n- [Hardmode, hardmode, H, h]\n- [Harder, harder, E, e]${NORMAL}\n"
     exit 1
   ;;
 esac
