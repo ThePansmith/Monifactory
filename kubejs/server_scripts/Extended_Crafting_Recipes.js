7f6876fe1e9072314e9f97b8f574c3ca854f9de6
IamNotBrianZ <74340257+IamNotBrianZ@users.noreply.github.com>
2024-05-29 14:21:21 +0800

    Update Extended_Crafting_Recipes.js

--- kubejs/server_scripts/Extended_Crafting_Recipes.js
+++ kubejs/server_scripts/Extended_Crafting_Recipes.js
@@ -204,4 +204,18 @@ ServerEvents.recipes(event => {
 		}
 	).id('kubejs:extended/exotic_materials_catalyst')
 
+	// Angel Ring
+	event.recipes.extendedcrafting.shaped_table(
+		'better_angel_ring:angel_ring', [
+            " AAA ",
+            "A B A",
+            "AC CA",
+            "A B A",
+            " AAA "
+		], {
+			A: 'gtceu:rose_gold_ingot',
+			B: Item.of('ironjetpacks:jetpack', '{Id:"ironjetpacks:reinforced"}').weakNBT(),
+			C: Item.of('ironjetpacks:jetpack', '{Id:"ironjetpacks:vibrant"}').weakNBT(),
+		} //, 2 // remove this comment to force t2 crafting only.
+	).id('kubejs:extended/angel_ring')
 })
