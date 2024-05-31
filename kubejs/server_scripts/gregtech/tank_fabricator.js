d5262a926c3e186d6a876752da1d935682517ff8
Alyx <75553966+JuiceyBeans@users.noreply.github.com>
2024-05-31 01:33:57 +0100

    Fix SDA

--- kubejs/server_scripts/gregtech/tank_fabricator.js
+++ kubejs/server_scripts/gregtech/tank_fabricator.js
@@ -1,4 +1,15 @@
 ServerEvents.recipes(event => {
+
+    function sda_print(input, circuit, output) { // PSA: DO NOT USE RESEVERED KEYWORDS FOR FUNCTIONS LMAO (this used to be called print)
+        event.recipes.gtceu.subatomic_digital_assembly(`kubejs:${input}_${circuit}`)
+            .notConsumable(`kubejs:${input}`)
+            .circuit(circuit)
+            .itemOutputs(output)
+            .CWUt(64)  // same for all recipes
+            .duration(500) // same for all recipes
+            .EUt(100000) // same for all recipes
+    }
+
     if (!isHardMode) {
         event.recipes.extendedcrafting.shaped_table(
             'gtceu:subatomic_digital_assembler', [
@@ -19,25 +30,16 @@ ServerEvents.recipes(event => {
         }
         ).id('kubejs:shaped/subatomic_digital_assembler')
 
-        function print(input, circuit, output) {
-            event.recipes.gtceu.subatomic_digital_assembly(`kubejs:${input}_${circuit}`)
-                .notConsumable(`kubejs:${input}`)
-                .circuit(circuit)
-                .itemOutputs(output)
-                .CWUt(64)  // same for all recipes
-                .duration(500) // same for all recipes
-                .EUt(100000) // same for all recipes
-        }
 
-    print('creative_tank_data', 1, 'gtceu:creative_tank')
-    print('creative_tank_data', 2, 'ae2:creative_fluid_cell')
-    print('creative_storage_data', 1, 'functionalstorage:max_storage_upgrade')
-    print('creative_storage_data', 2, 'gtceu:uhv_quantum_chest')
-    print('creative_storage_data', 3, 'gtceu:uhv_quantum_tank')
-    print('creative_data_hatch_data', 1, 'gtceu:creative_data_access_hatch')
-    print('creative_energy_data', 1, 'gtceu:creative_energy')
-    print('creative_energy_data', 2, 'enderio:creative_power')
-    print('creative_energy_data', 3, 'ae2:creative_energy_cell')
+        sda_print('creative_tank_data', 1, 'gtceu:creative_tank')
+        sda_print('creative_tank_data', 2, 'ae2:creative_fluid_cell')
+        sda_print('creative_storage_data', 1, 'functionalstorage:max_storage_upgrade')
+        sda_print('creative_storage_data', 2, 'gtceu:uhv_quantum_chest')
+        sda_print('creative_storage_data', 3, 'gtceu:uhv_quantum_tank')
+        sda_print('creative_data_hatch_data', 1, 'gtceu:creative_data_access_hatch')
+        sda_print('creative_energy_data', 1, 'gtceu:creative_energy')
+        sda_print('creative_energy_data', 2, 'enderio:creative_power')
+        sda_print('creative_energy_data', 3, 'ae2:creative_energy_cell')
 
         event.recipes.gtceu.subatomic_digital_assembly('kubejs:corrupted_data')
             .itemInputs('kubejs:universe_creation_data')
