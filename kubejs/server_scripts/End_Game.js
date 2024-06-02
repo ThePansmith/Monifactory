19c381a64f1533f7784e19cf7eb219afbcb1a68d
Levlion2 <121055316+Levlion2@users.noreply.github.com>
2024-06-02 18:29:16 +0100

    Revert "Added BBC and RHF assline recipes"
    This reverts commit ef4836d0e2e10bc07258d9439a00a8427c9b29b7.

--- kubejs/server_scripts/End_Game.js
+++ kubejs/server_scripts/End_Game.js
@@ -21,24 +21,6 @@ ServerEvents.recipes(event => {
             ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack('gtceu:large_chemical_reactor').EUt(7680).duration(600))
             .duration(1200)
             .EUt(131072)
-    //Rotary Hearth Furnace
-    event.remove({id:'gtceu:shaped/mega_blast_furnace'})
-    event.recipes.gtceu.assembly_line('mega_blast_furnace')
-            .itemInputs('gtceu:electric_blast_furnace','4x #gtceu:circuits/zpm','4x gtceu:luv_field_generator','4x gtceu:naquadah_alloy_spring','4x gtceu:dense_naquadah_alloy_plate','4x gtceu:uranium_rhodium_dinaquadide_quadruple_wire')
-            .inputFluids('gtceu:soldering_alloy 1152')
-            .itemOutputs('gtceu:mega_blast_furnace')
-            .stationResearch(b => b.researchStack('gtceu:electric_blast_furnace').CWUt(16, 64000).EUt(30720))
-            .duration(1200)
-            .EUt(30720)
-    // Bulk Blast Chiller
-    event.remove({id:'gtceu:shaped/mega_vacuum_freezer'})
-    event.recipes.gtceu.assembly_line('mega_vacuum_freezer')
-        .itemInputs('gtceu:vacuum_freezer','4x #gtceu:circuits/zpm','4x gtceu:luv_field_generator','4x gtceu:naquadah_normal_fluid_pipe','4x gtceu:dense_naquadah_alloy_plate','4x gtceu:uranium_rhodium_dinaquadide_quadruple_wire')
-        .inputFluids('gtceu:soldering_alloy 1152')
-        .itemOutputs('gtceu:mega_vacuum_freezer')
-        .stationResearch(b => b.researchStack('gtceu:vacuum_freezer').CWUt(16, 64000).EUt(30720))
-        .duration(1200)
-        .EUt(30720)
     
 
     // Blacklight
