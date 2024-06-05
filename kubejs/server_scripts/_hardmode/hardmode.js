adfe6f8d17af7e64e258017e74fddd72fc4d12e9
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-06-05 16:13:45 -0500

    misc qb fixes

--- kubejs/server_scripts/_hardmode/hardmode.js
+++ kubejs/server_scripts/_hardmode/hardmode.js
@@ -73,6 +73,7 @@ ServerEvents.recipes(event => {
         event.remove({id: "gtceu:large_chemical_reactor/radon_from_uranium_238"})
         event.remove({id: "gtceu:electric_blast_furnace/blast_sculk_compound_gas"})
         event.remove({id: "gtceu:circuit_assembler/wetware_board"})
+        event.remove({id: "enderio:stick"})
 
 // Anything that shouldn't apply to hardermode
         if (!isHarderMode) {
