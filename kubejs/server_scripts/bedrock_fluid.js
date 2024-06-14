c55090edfbbe55ab6b72b3128cf44426244e3113
ogmon1243 <83802482+ogmon1243@users.noreply.github.com>
2024-06-14 15:21:58 -0500

    Update bedrock_fluid.js

--- kubejs/server_scripts/bedrock_fluid.js
+++ kubejs/server_scripts/bedrock_fluid.js
@@ -2,7 +2,7 @@ GTCEuServerEvents.fluidVeins(event => {
 
     event.add('gtceu:custom_bedrock_fluid_vein', vein => {
         vein.dimensions('javd:void')
-        vein.fluid(() => Fluid.of("gtceu:oil").fluid)
+        vein.fluid(() => Fluid.of("gtceu:oil_medium").fluid)
         vein.weight(1000)
         vein.minimumYield(300)
         vein.maximumYield(300)
