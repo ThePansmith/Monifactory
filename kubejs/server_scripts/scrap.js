918e7e2a7353760f7814aaa86435ef7c670a9629
SparkedTheorem <162088357+SparkedTheorem@users.noreply.github.com>
2024-07-09 12:44:58 -0700

    Major Scrapline Buffs

--- kubejs/server_scripts/scrap.js
+++ kubejs/server_scripts/scrap.js
@@ -1,32 +1,53 @@
 ServerEvents.recipes(event => {
 
-
-
-    
     // Scrap
     event.recipes.gtceu.sifter('scrap_sifting')
         .itemInputs('kubejs:alien_scrap')
-        .chancedOutput('gtceu:stone_dust', 5000, 0)
-        .chancedOutput('gtceu:concrete_dust', 5000, 0)
-        .chancedOutput('gtceu:carbon_dust', 5000, 0)
-        .chancedOutput('kubejs:ruined_capacitor', 3300, 0)
-        .chancedOutput('kubejs:ruined_sensor', 3300, 0)
-        .chancedOutput('kubejs:ruined_emitter', 3300, 0)
+        .itemOutputs('kubejs:ruined_machine_parts')
+        .chancedOutput('kubejs:ruined_machine_parts', 3300, 200)
+        .itemOutputs('kubejs:ruined_hull')
+        .chancedOutput('kubejs:ruined_hull', 5000, 200)
+        .chancedOutput('kubejs:ruined_hull', 3300, 200)
+        .chancedOutput('gtceu:ruthenium_trinium_americium_neutronate_dust', 3300, 0)
         .duration(400)
         .EUt(16380)
+
+    // Ruined Machine Parts
+    event.recipes.gtceu.sifter('ruined_machine_sifting')
+        .itemInputs('kubejs:ruined_machine_parts')
+        .itemOutputs('gtceu:neutronium_dust')
+        .chancedOutput('gtceu:graphene_dust', 5000, 0)
+        .chancedOutput('gtceu:yttrium_barium_cuprate_dust', 5000, 0)
+        .chancedOutput('kubejs:ruined_capacitor', 5000, 0)
+        .chancedOutput('kubejs:ruined_sensor', 5000, 0)
+        .chancedOutput('kubejs:ruined_emitter', 5000, 0)
+        .duration(400)
+        .EUt(16380)
+    
+    // Ruined Hull
+        event.recipes.gtceu.centrifuge('ruined_hull_maceration')
+        .itemInputs('kubejs:ruined_hull')
+        .itemOutputs('5x gtceu:neutronium_dust')
+        .itemOutputs('3x gtceu:tritanium_dust')
+        .itemOutputs('4x gtceu:crystal_matrix_dust')
+        .chancedOutput('kubejs:holmium_compound', 3300, 0)
+        .chancedOutput('kubejs:magnetic_capacitor_dust', 3300, 0)
+        .chancedOutput('gtceu:holmium_oxide_dust', 3300, 0)
+        .duration(400)
+        .EUt(16380) 
 
 // Supercap
 // A simple "crush the thing and get the good stuff"
 event.recipes.gtceu.macerator('alien_scrap_supercap_crushing')
 .itemInputs('kubejs:ruined_capacitor')
-.itemOutputs('kubejs:crushed_capacitor')
+.chancedOutput('kubejs:crushed_capacitor', 9500, 0)
 .chancedOutput('gtceu:enriched_naquadah_trinium_europium_duranide_dust', 3300, 0)
 .duration(100)
 .EUt(16380)
 
 event.recipes.gtceu.electromagnetic_separator('alien_scrap_supercap_electro')
 .itemInputs('kubejs:crushed_capacitor')
-.itemOutputs('kubejs:magnetic_capacitor_dust')
+.chancedOutput('kubejs:magnetic_capacitor_dust', 9500, 0)
 .chancedOutput('gtceu:carbon_dust', 3300, 850) // Finally, an easy way to get carbon!
 .duration(100)
 .EUt(16380)
@@ -42,14 +63,14 @@ event.recipes.gtceu.centrifuge('alien_scrap_supercap_dust_centrifuge')
 // Ion exchange
 event.recipes.gtceu.macerator('alien_scrap_sensor_crushing')
 .itemInputs('kubejs:ruined_sensor')
-.itemOutputs('kubejs:crushed_sensor')
+.chancedOutput('kubejs:crushed_sensor', 9500, 0)
 .chancedOutput('gtceu:neutronium_dust', 3300, 850)
 .duration(600)
 .EUt(16380)
 
 event.recipes.gtceu.thermal_centrifuge('alien_scrap_sensor_centrifuge')
 .itemInputs('kubejs:crushed_sensor')
-.itemOutputs('kubejs:holmium_compound')
+.chancedOutput('kubejs:holmium_compound', 9500, 0)
 .chancedOutput('gtceu:silver_dust', 3300, 850)
 .duration(600)
 .EUt(16380)
@@ -66,7 +87,7 @@ event.recipes.gtceu.autoclave('holmium_compound_autoclave')
 // Has Glass containing holmium oxide
 event.recipes.gtceu.macerator('alien_scrap_emitter_crushing')
 .itemInputs('kubejs:ruined_emitter')
-.itemOutputs('kubejs:holmium_oxide_glass_dust')
+.chancedOutput('kubejs:holmium_oxide_glass_dust', 9500, 0)
 .chancedOutput('gtceu:trinium_dust', 3300, 850)
 .duration(400)
 .EUt(16380)
