076511d9f125d923215cf9a46a4ff2d9e8deee3d
IamNotBrianZ <74340257+IamNotBrianZ@users.noreply.github.com>
2024-07-14 00:43:27 +0800

    Add mote of omnium solidify recipe
    Withers said shes going to add this recipe
    And since this still isn't added, i decide to add it 
    
    NomiCEU parity

--- kubejs/server_scripts/mods/kubeJS.js
+++ kubejs/server_scripts/mods/kubeJS.js
@@ -178,6 +178,13 @@ ServerEvents.recipes(event => {
         .notConsumable('gtceu:ball_casting_mold')
         .duration(500)
         .EUt(16)
+    
+    event.recipes.gtceu.fluid_solidifier('mote_of_omnium')
+        .inputFluids(Fluid.of('gtceu:omnium', 16))
+        .itemOutputs('kubejs:mote_of_omnium')
+        .notConsumable('gtceu:ball_casting_mold')
+        .duration(20)
+        .EUt(7)
 
     //Thermal Fluids
     event.recipes.gtceu.extractor('primal_mana')
