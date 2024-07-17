b7bf5662fe0187e2833b2284f7fa1a3c5c06d01b
WithersChat <witherschat@gmail.com>
2024-07-17 05:10:10 +0200

    Multi-p2p recipe + SDA recipe fix

--- kubejs/server_scripts/gregtech/subatomic_digital_assembler.js
+++ kubejs/server_scripts/gregtech/subatomic_digital_assembler.js
@@ -35,6 +35,7 @@ ServerEvents.recipes(event => {
     sda_print('creative_data_hatch_data', 1, 'gtceu:creative_data_access_hatch', 256)
     sda_print('creative_energy_data', 1, 'enderio:creative_power', 256)
     sda_print('creative_energy_data', 2, 'ae2:creative_energy_cell', 256)
+    sda_print('creative_energy_data', 3, 'gtceu:creative_energy', 256)
 
     event.recipes.gtceu.subatomic_digital_assembly('kubejs:corrupted_data')
         .itemInputs('kubejs:universe_creation_data')
