6ea9d2dddbb49d6e12b452437cc8fe960ea3b44c
IamNotBrianZ <74340257+IamNotBrianZ@users.noreply.github.com>
2024-07-29 21:29:06 +0800

    Add stonecutting recipe for multi p2p
    QOL recipe

--- kubejs/server_scripts/random_recipes.js
+++ kubejs/server_scripts/random_recipes.js
@@ -599,6 +599,11 @@ ServerEvents.recipes(event => {
         event.stonecutting('ae2:'+type+'_p2p_tunnel', 'ae2:me_p2p_tunnel')
     })
 
+    const multi_p2p = ["redstone", "fluid", "fe"]
+    multi_p2p.forEach(type => {
+        event.stonecutting('mae2:'+type+'_multi_p2p_tunnel', 'mae2:item_multi_p2p_tunnel')
+    })
+    
     // Stonecutting CCI blocks
     let sameItemsTags = ['#chisel_chipped_integration:factory_block', '#chisel_chipped_integration:technical_block', '#chisel_chipped_integration:laboratory_block', '#chisel_chipped_integration:tyrian']; // What item tags to go through (change this so you have your tags)
     sameItemsTags.forEach(tag => {
