c9b2efe6ae1b9b94aa09931ea83256c93b4eae31
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-07-30 12:34:32 -0500

    updated moni multis

--- kubejs/server_scripts/mods/DraconicEvolution.js
+++ kubejs/server_scripts/mods/DraconicEvolution.js
@@ -11,7 +11,7 @@ ServerEvents.recipes(event => {
     event.recipes.gtceu.assembly_line('sculk_reaction_casing')
         .itemInputs('gtceu:assembly_line_unit', 'gtceu:enori_empowered_block', 'gtceu:europium_plate', '2x kubejs:mesol_core', '4x enderio:ender_crystal')
         .inputFluids('gtceu:soldering_alloy 1152')
-        .itemOutputs('moni_multiblocks:mesol_coil_block')
+        .itemOutputs('moni_multiblocks:mesol_voltic_casing')
         ["scannerResearch(java.util.function.UnaryOperator)"](b => b.researchStack('gtceu:enori_empowered_block').EUt(480).duration(1200))
         .duration(600)
         .EUt(30720)
