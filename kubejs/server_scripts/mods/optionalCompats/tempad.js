ae71e904a2ed343cf9894241f22da4a6fe47a1dd
Karthi Suresh <75553966+JuiceyBeans@users.noreply.github.com>
2024-08-05 11:54:34 +0400

    Add recipes to Tempads

--- kubejs/server_scripts/mods/optionalCompats/tempad.js
+++ kubejs/server_scripts/mods/optionalCompats/tempad.js
@@ -1,5 +1,5 @@
 if (Platform.isLoaded('tempad')) {
-    console.log("Tempad found and script loaded")
+    console.log("Tempad found, loading compat script")
     LootJS.modifiers((event) => {
         event.addLootTableModifier("minecraft:chests/simple_dungeon")
             .randomChance(0.02)
@@ -7,5 +7,27 @@ if (Platform.isLoaded('tempad')) {
     })
     ServerEvents.tags('item', event => {
         event.add('tempad:tempad_fuel', 'kubejs:moni_penny')
+    })
+    ServerEvents.recipes(event => {
+        event.shaped(
+            'tempad:tempad', [
+            'PFP',
+            'CNC',
+            'PSP'
+        ], {
+            P: 'gtceu:electrum_plate',
+            C: '#gtceu:circuits/mv',
+            F: 'gtceu:mv_field_generator',
+            S: 'gtceu:mv_sensor',
+            N: 'waystones:warp_stone'
+        }
+        ).id('tempad:tempad')
+
+        event.recipes.gtceu.assembler('tempad:he_who_remains_tempad')
+            .itemInputs('tempad:tempad', 'gtceu:lapotron_crystal', '4x #gtceu:circuits/ev', '16x kubejs:quantum_flux', '8x gtceu:tungsten_carbide_plate', '2x gtceu:signalum_plate')
+            .inputFluids('kubejs:primal_mana 1000')
+            .itemOutputs('tempad:he_who_remains_tempad')
+            .duration(100)
+            .EUt(512)
     })
 }
