e1f3bf30f768e3fb436cad3b47a56ca601165404
Xefyr <xefyr00@gmail.com>
2024-08-12 22:47:09 -0400

    More Utilitarian utility recipes to remove

--- kubejs/server_scripts/mods/utilitarian.js
+++ kubejs/server_scripts/mods/utilitarian.js
@@ -5,14 +5,16 @@ ServerEvents.recipes(event => {
     //Remove snad & variants
     event.remove({ output: ['utilitarian:snad', 'utilitarian:red_snad', 'utilitarian:soul_snad', 'utilitarian:fluid_hopper', 'utilitarian:no_soliciting_banner']})
 
-    //Remove utility recipes in harder modes
+    //Remove utility recipes when they overshadow GT recipes
+    event.remove({ id: 'utilitarian:utility/logs_to_hopper'})
     if(isHardMode) {
-        event.remove({ id: 'utilitarian:utility/logs_to_hopper'})
+        event.remove({ id: 'utilitarian:utility/charcoal_from_campfire'})
     }
     if(isHarderMode) {
         event.remove({ id: 'utilitarian:utility/logs_to_ladders'})
         event.remove({ id: 'utilitarian:utility/logs_to_chests'})
         event.remove({ id: 'utilitarian:utility/logs_to_sticks'})
         event.remove({ id: 'utilitarian:utility/logs_to_bowls'})
+        event.remove({ id: /utilitarian:.*_logs_to_.*/})
     }
 })
