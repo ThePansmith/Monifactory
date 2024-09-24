bb2953c0dc5196a1c530ef1f53e2760c03e051f5
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-09-24 18:35:28 -0500

    Update hatch_revert.js (#768)

--- kubejs/server_scripts/gregtech/hatch_revert.js
+++ kubejs/server_scripts/gregtech/hatch_revert.js
@@ -1,6 +1,5 @@
 ServerEvents.recipes(event => {
     if (!isHarderMode) {
-
         event.remove({ id: /item_import_bus_/})
         event.remove({ id: /item_export_bus_/})
         event.remove({ id: /fluid_hatch_/, type: 'gtceu:assembler'})
