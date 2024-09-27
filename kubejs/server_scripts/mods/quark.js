90f13e2cf9495d6bce11690d67d45fd4a73b682b
OE100 <omer.er10@gmail.com>
2024-09-27 21:40:12 +0300

    add recipes for missing quark polished stone types

--- kubejs/server_scripts/mods/quark.js
+++ kubejs/server_scripts/mods/quark.js
@@ -8,4 +8,40 @@ ServerEvents.recipes(event => {
             T: 'quark:polished_tuff'
         }
     ).id('kubejs:shaped/quark_tuff_bricks')
+
+    event.shaped(
+        '4x quark:polished_jasper', [
+            'TT',
+            'TT'
+        ], {
+            T: 'quark:jasper'
+        }
+    ).id('kubejs:shaped/quark_polished_jasper')
+
+    event.shaped(
+        '4x quark:polished_limestone', [
+            'TT',
+            'TT'
+        ], {
+            T: 'quark:limestone'
+        }
+    ).id('kubejs:shaped/quark_polished_limestone')
+
+    event.shaped(
+        '4x quark:polished_shale', [
+            'TT',
+            'TT'
+        ], {
+            T: 'quark:shale'
+        }
+    ).id('kubejs:shaped/quark_polished_shale')
+
+    event.shaped(
+        '4x quark:polished_myalite', [
+            'TT',
+            'TT'
+        ], {
+            T: 'quark:myalite'
+        }
+    ).id('kubejs:shaped/quark_polished_myalite')
 })
