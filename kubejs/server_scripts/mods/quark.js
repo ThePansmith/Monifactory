5aa8fbbcfeb6246873227d82a61e43c6ef8774b2
OE100 <omer.er10@gmail.com>
2024-10-02 02:00:08 +0300

    removed tuff bricks recipe as that was fixed in another pr

--- kubejs/server_scripts/mods/quark.js
+++ kubejs/server_scripts/mods/quark.js
@@ -1,15 +1,6 @@
 ServerEvents.recipes(event => {
     // add missing recipes
     event.shaped(
-        '4x quark:tuff_bricks', [
-            'TT',
-            'TT'
-        ], {
-            T: 'quark:polished_tuff'
-        }
-    ).id('kubejs:shaped/quark_tuff_bricks')
-
-    event.shaped(
         '4x quark:polished_jasper', [
             'TT',
             'TT'
