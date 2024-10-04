65763a398dc5040a231a40f87d227ba4e7d871a0
Xefyr0 <xefyr00@gmail.com>
2024-10-04 14:03:17 -0400

    Add Advanced Laser Connector and Energy Card, but reasonably gated

--- kubejs/client_scripts/JEI.js
+++ kubejs/client_scripts/JEI.js
@@ -190,7 +190,7 @@ JEIEvents.hideItems(event => {
     event.hide('gtceu:steel_machine_casing')
 
     //Laserio
-    event.hide(['laserio:card_energy', 'laserio:laser_connector_advanced', 'laserio:logic_chip_raw', 'laserio:logic_chip'])
+    event.hide(['laserio:logic_chip_raw', 'laserio:logic_chip'])
 
     // PEX
     event.hide(['packagedexcrafting:flux_crafter', 'packagedexcrafting:basic_crafter'])
