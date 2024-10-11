a879cf31e9a94fb895a9089ae96b0957d4307153
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-10-11 13:57:59 -0500

    Close #891

--- kubejs/server_scripts/normal_mode.js
+++ kubejs/server_scripts/normal_mode.js
@@ -184,9 +184,6 @@ ServerEvents.recipes(event => {
     }
     )
     
-    event.remove({ output: 'systeams:stirling_boiler' })
-    event.shapeless('systeams:stirling_boiler', ['steamdynamo:steam_dynamo', 'systeams:boiler_pipe'])
-
     //Bounty board recipes only accept oak. The dev has stated this is intended. https://github.com/ejektaflex/Bountiful/issues/271
     event.replaceInput({ id: "bountiful:crafting/bountyboard" }, "minecraft:oak_log", "#minecraft:logs")
     event.replaceInput({ id: "bountiful:crafting/bountyboard" }, "minecraft:oak_planks", "#minecraft:planks")
