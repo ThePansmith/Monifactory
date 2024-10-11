d8289aca15605cd4cda4e2cb36448492682acac9
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-10-11 15:07:42 -0500

    Soph Storage recipe fixes

---
+++ kubejs/server_scripts/unification/other_tags.js
@@ -0,0 +1,5 @@
+ServerEvents.tags('item', event => {
+    // Random small tags, just to keep EMI clean
+    event.add('moni:SophContainer', ["#forge:barrels/wooden", "#forge:chests/wooden"])
+	event.add('moni:SophGemBlock', ["#forge:storage_blocks/diamond", "#forge:storage_blocks/emerald"])
+})
