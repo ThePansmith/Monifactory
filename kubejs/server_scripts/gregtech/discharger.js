40fcca4619ad8fbce94e8492ffa37a7cd0a70c63
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2024-10-13 14:08:45 -0400

    Rebalance Solars (#924)
    * Make solars scale more uniformly
    
    * Adjust solar panel recipes to match new output values
    
    * Adjustment to Infinity solars to match the intended order of magnitude
    
    * Remove Neutronium conversion recipes to prevent converting between Hadal and Neutronium solars
    
    * Adjust & improve Assembler recipes for Solars
    
    * Add a bit to the comment on the solar flux script

--- kubejs/server_scripts/gregtech/discharger.js
+++ kubejs/server_scripts/gregtech/discharger.js
@@ -32,8 +32,8 @@ ServerEvents.recipes(event => {
 
     Discharge('hadal_solar', '2x solarflux:sp_custom_hadal', ['kubejs:hadal_energy_core', '2x solarflux:sp_custom_abyssal', '2x kubejs:abyssal_energy_core'], 1000000)
     Discharge('nt_solar', 'solarflux:sp_custom_neutronium', ['solarflux:sp_custom_hadal', '4x kubejs:hadal_energy_core', '4x gtceu:sculk_superconductor_single_wire', '2x gtceu:omnium_block'], 100000000)
-    Discharge('abyssal_solar', '2x solarflux:sp_custom_abyssal', ['kubejs:abyssal_energy_core', '4x solarflux:sp_custom_bathyal', '4x enderio:sculk_superconductor_conduit', '2x kubejs:bathyal_energy_core'], 20000000)
-    Discharge('bathyal_solar', '2x solarflux:sp_custom_bathyal', ['kubejs:bathyal_energy_core', '4x solarflux:sp_8', '4x enderio:cryolobus_conduit', '2x gtceu:cryolobus_block'], 20000000)
+    Discharge('abyssal_solar', '2x solarflux:sp_custom_abyssal', ['kubejs:abyssal_energy_core', '2x solarflux:sp_custom_bathyal', '4x enderio:sculk_superconductor_conduit', '2x kubejs:bathyal_energy_core'], 20000000)
+    Discharge('bathyal_solar', '2x solarflux:sp_custom_bathyal', ['kubejs:bathyal_energy_core', '2x solarflux:sp_8', '4x enderio:cryolobus_conduit', '2x gtceu:cryolobus_block'], 20000000)
     Discharge('cryococcus_block', '5x gtceu:cryococcus_block', ['5x gtceu:cryolobus_block', '4x kubejs:bathyal_energy_core', '2x kubejs:warden_heart'], 40000000)
     Discharge('hadal_warp_engine', 'kubejs:hadal_warp_engine', ['gtceu:cryococcus_frame', 'kubejs:warp_engine', 'gtceu:cryococcus_plate', '2x gtceu:cryolobus_plate', 'kubejs:hadal_energy_core', 'gtceu:zpm_field_generator', 'kubejs:abyssal_energy_core', 'kubejs:hadal_shard'], 40000000)
 })
