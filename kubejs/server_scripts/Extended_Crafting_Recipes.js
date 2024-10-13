de153ca5df9eb6d8803719b3d277ff256f38b7fb
Xefyr0 <xefyr00@gmail.com>
2024-10-13 12:41:28 -0400

    Change the Angel Ring recipe and quests to match the intended recipe

--- kubejs/server_scripts/Extended_Crafting_Recipes.js
+++ kubejs/server_scripts/Extended_Crafting_Recipes.js
@@ -231,7 +231,7 @@ ServerEvents.recipes(event => {
 		], {
 			A: 'gtceu:rose_gold_ingot',
 			B: Item.of('ironjetpacks:jetpack', '{Id:"ironjetpacks:energetic"}').weakNBT(),
-			C: Item.of('ironjetpacks:jetpack', '{Id:"ironjetpacks:electrical_steel"}').weakNBT(),
+			C: Item.of('ironjetpacks:jetpack', '{Id:"ironjetpacks:reinforced"}').weakNBT(),
 		} //, 2 // remove this comment to force t2 crafting only.
 	).id('kubejs:extended/angel_ring')
 })
