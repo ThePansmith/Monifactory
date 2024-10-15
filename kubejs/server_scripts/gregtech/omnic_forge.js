658d511a87a00f9ea966390636e6f348f6046b8c
Xefyr0 <xefyr00@gmail.com>
2024-10-15 13:52:24 -0400

    Move omnic forge recipes into existing file + make FSOPC slightly cheaper

--- kubejs/server_scripts/gregtech/omnic_forge.js
+++ kubejs/server_scripts/gregtech/omnic_forge.js
@@ -115,4 +115,16 @@ ServerEvents.recipes(event => {
         .itemOutputs('64x kubejs:quantum_flux')
         .duration(50)
         .EUt(1920)
+    
+    event.recipes.gtceu.omnic_forge('fieldstabilizedcompound')
+        .itemInputs('4x gtceu:uiv_field_generator', '3x gtceu:uev_field_generator', '2x gtceu:uhv_field_generator',  '12x gtceu:omnic_acid_dust', '10x kubejs:quasi_stable_neutron_star')
+        .itemOutputs('kubejs:field_stabilised_omnic_pulsar_compound')
+        .duration(255)
+        .EUt(16380)
+
+    event.recipes.gtceu.omnic_forge('singularity_containment_unit')
+        .itemInputs('4x gtceu:monium_plate', '4x gtceu:neutron_reflector', '1x gtceu:uev_sensor', '2x gtceu:uhv_sensor', 'gtceu:uev_field_generator')
+        .itemOutputs('64x kubejs:singularity_containment_unit')
+        .duration(300)
+        .EUt(16380)
 })
