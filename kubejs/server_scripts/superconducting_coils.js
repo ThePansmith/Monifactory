eecb150d081889b8a15fa0b8f0ed4f2b9e2f4691
Xefyr0 <xefyr00@gmail.com>
2024-11-02 21:14:30 -0400

    Change trinium requirement to be halved instead of quartered

--- kubejs/server_scripts/superconducting_coils.js
+++ kubejs/server_scripts/superconducting_coils.js
@@ -1,10 +1,10 @@
 /**
  * A little bit of software wizardry that alters any "superconducting_coil" recipes in the Assembler
- * to use 1/4 of the Trinium they normally do.
+ * to use 1/2 of the Trinium they normally do.
  */
 ServerEvents.recipes(event=> {
     //Get all GTCEu Assembler recipes with an ID matching the regex
-    event.forEachRecipe({ id: /gtceu:assembler\/superconducting_coil/, type: "gtceu:assembler" }, supercon_coil_recipe => {
+    event.findRecipes({ id: /gtceu:assembler\/superconducting_coil/, type: "gtceu:assembler" }).forEach(supercon_coil_recipe => {
         //Get the JSON array object representing all of the fluid ingredients
         let fluidIngredients = supercon_coil_recipe.json.getAsJsonObject("inputs").getAsJsonArray("fluid")
         for (let i = 0; i < fluidIngredients.size(); i++) {
@@ -18,9 +18,9 @@ ServerEvents.recipes(event=> {
             let fluidIngredient = curFluidIngredient.getAsJsonArray("value")
             for (let j = 0; j < fluidIngredient.size(); j++) {
                 if(fluidIngredient.get(j).getAsJsonPrimitive("tag").asString == "forge:trinium") {
-                    //Change fluid ingredient to 1/4 the amount if it does match
+                    //Change fluid ingredient to 1/2 the amount if it does match
                     curFluidIngredient.remove("amount")
-                    curFluidIngredient["addProperty(java.lang.String,java.lang.Number)"]("amount", fluidAmount / 4)   
+                    curFluidIngredient["addProperty(java.lang.String,java.lang.Number)"]("amount", fluidAmount / 2)   
                 }
             }
         }
