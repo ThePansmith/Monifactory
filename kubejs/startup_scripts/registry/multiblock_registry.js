9bb17165bb6eaff7ee4c4634a495772480fcd2ee
TehBanana <90364148+teh-banana@users.noreply.github.com>
2024-11-09 20:30:13 +1000

    Allow Greenhouse to show all it's output on some recipes

--- kubejs/startup_scripts/registry/multiblock_registry.js
+++ kubejs/startup_scripts/registry/multiblock_registry.js
@@ -111,7 +111,7 @@ GTCEuStartupEvents.registry('gtceu:recipe_type', event => {
     event.create('greenhouse')
         .category('multiblock')
         .setEUIO('in')
-        .setMaxIOSize(3, 3, 1, 0)
+        .setMaxIOSize(3, 6, 1, 0)
         .setSlotOverlay(false, false, GuiTextures.SOLIDIFIER_OVERLAY)
         .setProgressBar(GuiTextures.PROGRESS_BAR_ARROW, FillDirection.LEFT_TO_RIGHT)
         .setSound(GTSoundEntries.TURBINE)
