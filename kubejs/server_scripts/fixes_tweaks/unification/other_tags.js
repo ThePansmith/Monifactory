edac5f8a593917488e5c31ccb48fbdbc0e9b9e3d
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2024-11-10 11:10:16 -0500

    Sophisticated Storage rework (#1002)
    * Sophisticated Storage rework
    - Both backpacks and storages now follow the same unified GT/TE progression, albeit with different amounts of the same materials
    - Storages are now crafted directly from the previous tier like backpacks
    - All tiers of backpack and storage are now craftable
    - Limited barrels and Shulkers are properly obtainable and appear in JEI
    - Textures & lang updated to reflect all of the above changes
    - All recipes show properly in JEI (or at least to the degree that the mod does regularly)
    - Compacting upgrades limited to EM only and are now obtainable for other modes
    - Replaced moni:gemblock and moni:sophcontainer tags with preexisting tags
    - Tier upgrade items and related tooltips are removed & hidden, but still functional for people who made them
    
    * Remove limited barrels
    
    * Significantly reduce the combinatorial explosion of SophStorage blocks shown in EMI
    
    * Actually make Limited Barrels unobtainable this time
    
    * Rework sophstorage tiers to nuke Iron tier
    
    * Separate backpack tiering from barrel tiering to keep iron tier as it was before this rework
    
    * re-added tier upgrade items, but all applicable permutations this time

--- kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
+++ kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
@@ -1,5 +1,3 @@
 ServerEvents.tags('item', event => {
-    // Random small tags, just to keep EMI clean
-    event.add('moni:sophcontainer', ["#forge:barrels/wooden", "#forge:chests/wooden"])
-	event.add('moni:sophgemblock', ["#forge:storage_blocks/diamond", "#forge:storage_blocks/emerald"])
+    // Random small tags that don't fit anywhere else
 })
