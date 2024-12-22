567324d21a06f55d53634b4a3efa591c99aac294
Tom Prince <tom.prince@hocat.ca>
2024-12-21 16:38:02 -0800

    Remove tag additions that are now included by default.

--- kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
+++ kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
@@ -3,6 +3,4 @@ ServerEvents.tags('item', event => {
 })
 
 ServerEvents.tags('block', event => {
-    event.add('gtceu:cleanroom_floors', 'enderio:travel_anchor');
-    event.add('gtceu:cleanroom_floors', '#elevatorid:elevators');
 })
