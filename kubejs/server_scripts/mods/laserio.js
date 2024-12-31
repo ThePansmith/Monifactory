8dc65a5f1f4a379182ea44607035027efe5a5f72
Pansmith <70342772+ThePansmith@users.noreply.github.com>
2024-12-31 09:00:49 -0600

    Update laserio.js

--- kubejs/server_scripts/mods/laserio.js
+++ kubejs/server_scripts/mods/laserio.js
@@ -10,7 +10,7 @@ ServerEvents.recipes(event => {
 
     //Replace Logic chips with circuits.
     event.remove({ output: ['laserio:logic_chip_raw', 'laserio:logic_chip'] })
-    event.replaceInput({ mod: 'laserio', not: [{ id: 'laserio:card_item' }, { id: 'laserio:card_fluid' }, { id: 'laserio:card_energy' }, { id: 'laserio:card_redstone' }] }, 'laserio:logic_chip', 'cardChip')
+    event.replaceInput({ mod: 'laserio', not: [{ id: 'laserio:card_item' }, { id: 'laserio:card_fluid' }, { id: 'laserio:card_energy' }, { id: 'laserio:card_redstone' }] }, 'laserio:logic_chip', cardChip)
     
     const Cards = [
         ['item', 'gtceu:pulsating_alloy_plate'],
