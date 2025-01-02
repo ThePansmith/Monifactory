6a2b260df0e154402470ce71ccdae03a52892a61
Xefyr0 <xefyr00@gmail.com>
2025-01-01 22:15:47 -0500

    Make Ad Astra ores mineable with a pickaxe

--- kubejs/startup_scripts/oreVeins.js
+++ kubejs/startup_scripts/oreVeins.js
@@ -1,3 +1,5 @@
+const BlockTags = Java.loadClass("net.minecraft.tags.BlockTags")
+
 GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
     event.create('moon')
         .targets('#ad_astra:moon_stone_replaceables')
@@ -7,11 +9,11 @@ GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
         .targets('#ad_astra:mars_stone_replaceables')
         .dimensions('ad_astra:mars')
 
-        event.create('mercury')
+    event.create('mercury')
         .targets('#ad_astra:mercury_stone_replaceables')
         .dimensions('ad_astra:mercury')
 
-        event.create('venus')
+    event.create('venus')
         .targets('#ad_astra:venus_stone_replaceables')
         .dimensions('ad_astra:venus')
 })
@@ -23,25 +25,29 @@ GTCEuStartupEvents.registry('gtceu:tag_prefix', event => {
         .unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
         .generationCondition(ItemGenerationCondition.hasOreProperty)
+        .miningToolTag('forge:mineable/pickaxe')
 
-        event.create('mars', 'ore')
+    event.create('mars', 'ore')
         .stateSupplier(() => Block.getBlock('ad_astra:mars_stone').defaultBlockState())
         .baseModelLocation('ad_astra:block/mars_stone')
         .unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
         .generationCondition(ItemGenerationCondition.hasOreProperty)
+        .miningToolTag('forge:mineable/pickaxe')
 
-        event.create('mercury', 'ore')
+    event.create('mercury', 'ore')
         .stateSupplier(() => Block.getBlock('ad_astra:mercury_stone').defaultBlockState())
         .baseModelLocation('ad_astra:block/mercury_stone')
         .unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
         .generationCondition(ItemGenerationCondition.hasOreProperty)
+        .miningToolTag('forge:mineable/pickaxe')
 
-        event.create('venus', 'ore')
+    event.create('venus', 'ore')
         .stateSupplier(() => Block.getBlock('ad_astra:venus_stone').defaultBlockState())
         .baseModelLocation('ad_astra:block/venus_stone')
         .unificationEnabled(true)
         .materialIconType(GTMaterialIconType.ore)
         .generationCondition(ItemGenerationCondition.hasOreProperty)
+        .miningToolTag('forge:mineable/pickaxe')
 })
