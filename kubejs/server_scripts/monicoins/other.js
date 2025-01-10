9ccd3435e781305f72bb887e7c75dd742538abf7
Xefyr0 <xefyr00@gmail.com>
2025-01-10 09:19:14 -0500

    Make Air Affinity purchasable with Monicoins

--- kubejs/server_scripts/monicoins/other.js
+++ kubejs/server_scripts/monicoins/other.js
@@ -1,6 +1,13 @@
 ServerEvents.recipes(event => {
     if (isNormalMode) {
         //-- Other stuff
-
+        event.shaped(Item.of(Item.of('minecraft:enchanted_book').enchant('ensorcellation:air_affinity', 1), 1), [
+            ' A ',
+            'A A',
+            ' A '
+        ], {
+            A: 'kubejs:moni_dollar'
+        }).noMirror().noShrink()
+        
     }
 })
