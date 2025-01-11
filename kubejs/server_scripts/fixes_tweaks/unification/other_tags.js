a6c733a43d1b65285ea36ec5d6f0b284b501de7a
Xefyr0 <xefyr00@gmail.com>
2025-01-11 18:35:24 -0500

    Make Quarktech immune to Powdered Snow

--- kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
+++ kubejs/server_scripts/fixes_tweaks/unification/other_tags.js
@@ -1,5 +1,6 @@
 // Random small tags that don't fit anywhere else
 ServerEvents.tags('item', event => {
+    event.add("minecraft:freeze_immune_wearables", "gtceu:quarktech_boots")
 })
 
 ServerEvents.tags('block', event => {
