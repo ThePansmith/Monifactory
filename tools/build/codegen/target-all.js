866566a09d177961774ae4a3bb43ce827e129f90
iluha168 <illya.chernyshov888@gmail.com>
2025-01-18 12:31:40 +0700

    Make it possible to choose a specific codegen target from build cli

--- tools/build/codegen/target-all.js
+++ tools/build/codegen/target-all.js
@@ -1,10 +1,12 @@
 import Juke from "../juke/index.js"
 
-import CodegenCreditsTarget from "./credits/target"
-import CodegenLangsTarget from "./langs/target"
+import { CodegenCreditsTarget } from "./credits/target.js"
+import { CodegenLangsTarget } from "./langs/target.js"
 
 export const CodegenAllTarget = new Juke.Target({
     dependsOn: [CodegenCreditsTarget, CodegenLangsTarget]
 })
+
+export { CodegenCreditsTarget, CodegenLangsTarget }
 
 export default CodegenAllTarget
