616f0d4ff7fe2124538ed5c4f34186a63c90fe54
iluha168 <illya.chernyshov888@gmail.com>
2025-01-21 17:21:08 +0700

    better use in cli

--- pack-mode-switcher.sh
+++ pack-mode-switcher.sh
@@ -10,30 +10,36 @@ NORMAL=$(tput sgr0)
 
 touch .mode
 set -e
-printf "${POWDER_BLUE}Monifactory | Pack Mode Switcher${NORMAL}"
 
 NORMAL_CFG='./config-overrides/normal'
 HARDMODE_CFG='./config-overrides/hardmode'
 EXPERT_CFG='./config-overrides/expert'
 TARGET='./config'
-CURRENT_MODE="$(head .mode)"
-CURRENT_MODE=${CURRENT_MODE:="normal"}
 
 # Check if config-overrides dir exists
-if [[ ! -d "${NORMAL_CFG}" ]] || [[ ! -d "${HARDMODE_CFG}" ]] || [[ ! -d "${EXPERT_CFG}" ]] ; then
+if ! ([ -d "${NORMAL_CFG}" ] && [ -d "${HARDMODE_CFG}" ] && [ -d "${EXPERT_CFG}" ]); then
   printf "\n\n${RED}Could not find \`config-overrides\` directory! \nMake sure you are in the \`/minecraft\` directory of your instance! (The one containing \`/config\`)${NORMAL}\n"
   printf "${YELLOW}Otherwise, if you are in the \`/minecraft\` directory, please try reinstalling the pack.${NORMAL}\n"
   exit 1
 fi
 
-# Capitalise First Letter (only works in bash 4+)
-[ "${BASH_VERSINFO:-0}" -ge 4 ] && CURRENT_MODE=${CURRENT_MODE^}
-
-printf "\n\n${YELLOW}Current Mode: ${CURRENT_MODE}${NORMAL}\n"
-
 if [ -z "$1" ]; then
-  printf "${POWDER_BLUE}Set Pack Mode:\nN: Normal    (The Default mode) \nH: Hardmode  (Adds more lines and progression, removes HNN and Monicoin spending) \nE: Expert    (A modifier for Hardmode, enables some of the more extreme GTm settings among other things) \nSelection: [Normal / Hardmode / Expert]:"
-  read MODE
+  CURRENT_MODE="$(head .mode)"
+  CURRENT_MODE=${CURRENT_MODE:="normal"}
+  # Capitalise First Letter (only works in bash 4+)
+  [ "${BASH_VERSINFO:-0}" -ge 4 ] && CURRENT_MODE=${CURRENT_MODE^}
+
+  if [ -t 0 ]; then
+    # Interactive stdin
+    printf "${POWDER_BLUE}Monifactory | Pack Mode Switcher${NORMAL}"
+    printf "\n\n${YELLOW}Current Mode: ${CURRENT_MODE}${NORMAL}\n"
+    printf "${POWDER_BLUE}Set Pack Mode:\nN: Normal    (The Default mode) \nH: Hardmode  (Adds more lines and progression, removes HNN and Monicoin spending) \nE: Expert    (A modifier for Hardmode, enables some of the more extreme GTm settings among other things) \nSelection: [Normal / Hardmode / Expert]:"
+    read MODE
+  else
+    # Non interactive stdin
+    echo $CURRENT_MODE
+    exit 0
+  fi
 else
   MODE="$1"
 fi
@@ -90,4 +96,10 @@ case $MODE in
   ;;
 esac
 
-printf "\n${GREEN}Complete!${NORMAL}\n"
+if [ -t 0 ]; then
+  # Interactive stdin
+  printf "\n${GREEN}Complete!${NORMAL}\n"
+else
+  # Explicitly signify success
+  exit 0
+fi
