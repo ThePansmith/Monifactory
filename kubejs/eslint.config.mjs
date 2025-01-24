16760118cc6f92fa239ce97c7450c25531a60478
iluha168 <illya.chernyshov888@gmail.com>
2025-01-24 18:03:53 +0700

    eslint: enforce styles from CONTRIBUTING.md

--- kubejs/eslint.config.mjs
+++ kubejs/eslint.config.mjs
@@ -1,15 +1,22 @@
 import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin-js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  js.configs.recommended,
-  {
-    rules: {
-      "no-unused-vars": "off",
-      "no-undef": "off",
-      "no-unexpected-multiline": "off",
-      "no-var": "error",
-      "no-useless-escape": "warn",
+    js.configs.recommended,
+    {
+        plugins: {
+            "@stylistic/js": stylistic,
+        },
+        rules: {
+            "no-unused-vars": "off",
+            "no-undef": "off",
+            "no-unexpected-multiline": "off",
+            "no-var": "error",
+            "no-useless-escape": "warn",
+            "@stylistic/js/indent": ["error", 4],
+            "@stylistic/js/spaced-comment": "error",
+            "@stylistic/js/linebreak-style": ["error", "windows"],
+        },
     },
-  },
 ];
