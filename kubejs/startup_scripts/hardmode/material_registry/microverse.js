4b7936aaf1eb5491baaa07bcbb3decec3509cea9
Xefyr0 <xefyr00@gmail.com>
2025-01-26 02:24:18 -0500

    Color Darmstadite and Dulysite, and make Dulysite a gem

--- kubejs/startup_scripts/hardmode/material_registry/microverse.js
+++ kubejs/startup_scripts/hardmode/material_registry/microverse.js
@@ -6,12 +6,14 @@ GTCEuStartupEvents.registry('gtceu:material', event => {
     event.create("darmstadtite") // Hardmode only
         .dust().ore(2, 1)
         .iconSet('dull')
+        .color(0x99AA87)
         .components('2x darmstadtium', '3x sulfur')
         .addOreByproducts('rare_earth', 'rhodium_sulfate', 'darmstadtium')
 
     event.create("dulysite") // Hardmode only
-        .dust().ore(2, 1)
-        .iconSet('dull')
+        .gem().ore(2, 1)
+        .iconSet(GTMaterialIconSet.DIAMOND)
+        .color(0xF5EFC0)
         .components('duranium', '3x chlorine')
         .addOreByproducts('sphalerite', 'duranium', 'europium')
 
