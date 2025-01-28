98b8dffb4d99a903a694540ee11ca83c0d45b013
Tom Prince <tom.prince@hocat.ca>
2025-01-28 16:25:29 -0700

    Remove pristine matter, and make stabilized miner missions take inputs.
    Also, make the stabilized miner cheaper (hadal shards and iridium plates
    in half, field generators by 2/3) to compensate for the additional cost
    to run them.

--- kubejs/server_scripts/microverse/mission_utils.js
+++ kubejs/server_scripts/microverse/mission_utils.js
@@ -1,7 +1,6 @@
 // priority: 1
 /**
  * Utility functions for registering microminer missions,
- * actualization chamber counterparts,
  * and lower-tier projector missions more succinctly.
  */
 
@@ -71,7 +70,7 @@ const missionEUt = {
 }
 
 /**
- * Registers a basic microverse mission and equivalent actualization chamber recipe
+ * Registers a basic microverse mission and equivalent stabilized mission recipe
  * @param {Internal.RecipesEventJS} event Parameter used in consumer for ServerEvents.recipes().
  * @param {number|string} minerTier Miner tier index. Typically 1 through 12.
  * @param {number} duration Recipe duration in seconds. Defaults based on the miner tier if left undefined.
@@ -110,12 +109,12 @@ function microverse_mission(event, minerTier, projectorTier, duration, EUt, mine
     }
 
     // Register actualization chamber counterparts in Hard Mode and Expert Mode except for T9+
-    if(isHardMode && minerTier < 9 || minerTier == "2half" || minerTier == "4half" || minerTier == "8half") {
-        builders[1] = event.recipes.gtceu.actualization_chamber(`kubejs:pristine_t${minerTier}_${global.mission_counts[minerTier]}`)
-            .itemInputs(`kubejs:pristine_matter_t${minerTier}`)
-            .circuit(global.mission_counts[minerTier])
-            .duration(800)
-            .EUt(GTValues.VA[GTValues.LuV])
+    if(isHardMode && minerTier < 9 || (typeof minerTier === "string" && minerTier.replace("half", "") < 9)) {
+        builders[1] = event.recipes.gtceu.microverse(`kubejs:mission_t${minerTier}_${global.mission_counts[minerTier]}_stabalized`)
+            .addData("projector_tier", projectorTier)
+            .notConsumable(`kubejs:stabilized_microminer_t${minerTier}`)
+            .duration(Math.round(duration * 20))
+            .EUt(EUt)
     }
 
     return builders;
--- kubejs/startup_scripts/registry/item_registry.js
+++ kubejs/startup_scripts/registry/item_registry.js
@@ -19,35 +19,25 @@ StartupEvents.registry("item", event => {
         event.create("damaged_microminer_t4half").maxStackSize(16).textureJson({ layer0: "kubejs:item/microverse/microminer_t4half", layer1: "minecraft:block/destroy_stage_2" })
         event.create("microminer_t8half").maxStackSize(16).texture("kubejs:item/microverse/microminer_t8half")
         event.create("damaged_microminer_t8half").maxStackSize(16).textureJson({ layer0: "kubejs:item/microverse/microminer_t8half", layer1: "minecraft:block/destroy_stage_2" })
-    }
 
-
-    // Pristine Matters
-    const pristine_matter = [
-        ["1", "#c0c0b0"],
-        ["2", "#f5f5f1"],
-        ["2half", "#414751"],
-        ["3", "#635480"],
-        ["4", "#f17d3d"],
-        ["4half", "#eef487"],
-        ["5", "#fccc6a"],
-        ["6", "#58a14e"],
-        ["7", "#173639"],
-        ["8", "#dcdbe4"],
-        ["8half", "#4ebcef"]
-    ]
-
-    if (!isNormalMode) {
-        for (const [tier, color] of pristine_matter) {
+        // Stabilized Miners
+        for (const tier of [
+            "1",
+            "2",
+            "2half",
+            "3",
+            "4",
+            "4half",
+            "5",
+            "6",
+            "7",
+            "8",
+            "8half",
+        ]) {
             event.create(`stabilized_microminer_t${tier}`)
                 .maxStackSize(16)
                 .texture(`kubejs:item/microverse/microminer_t${tier}`)
                 .glow(true);
-            event.create(`pristine_matter_t${tier}`)
-                .textureJson({ layer0: "kubejs:item/prediction" })
-                .color(0, color)
-                .glow(true);
-
         }
     }
 
