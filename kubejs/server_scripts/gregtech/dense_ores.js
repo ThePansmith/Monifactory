8fa68cfd00570e49a1d3c0c718326cb31a325305
Tom Prince <tom.prince@hocat.ca>
2025-01-29 14:00:38 -0700

    Use GT materials for dense ore decomposition.
    Also adjust output amounts to account for original ore type.

--- kubejs/server_scripts/gregtech/dense_ores.js
+++ kubejs/server_scripts/gregtech/dense_ores.js
@@ -3,22 +3,22 @@
  */
 
 const ores = [
-    {name: 'redstone', mod: 'gtceu'},
-    {name: 'diamond', mod: 'gtceu'},
-    {name: 'emerald', mod: 'gtceu'},
-    {name: 'lapis', mod: 'gtceu'},
-    {name: 'coal', mod: 'gtceu'},
-    {name: 'nether_quartz', mod: 'gtceu'},
-    {name: 'iron', mod: 'minecraft'},
-    {name: 'gold', mod: 'minecraft'},
-    {name: 'copper', mod: 'minecraft'},
+    ['redstone', 16],
+    ['diamond', 16],
+    ['emerald', 16],
+    ['gold', 16],
+    ['lapis', 16],
+    ['iron', 16],
+    ['coal', 16],
+    ['nether_quartz', 32],
+    ['copper', 16],
 ]
 
 ServerEvents.recipes(event => {
-    ores.forEach(ore => {
-        event.recipes.gtceu.chemical_reactor(`kubejs:undensify_${ore.name}`)
-            .itemInputs(`kubejs:dense_${ore.name}_ore`)
-            .itemOutputs(`16x ${ore.mod}:raw_${ore.name}`)
+    ores.forEach(([name, amount]) => {
+        event.recipes.gtceu.chemical_reactor(`kubejs:undensify_${name}`)
+            .itemInputs(`kubejs:dense_${name}_ore`)
+            .itemOutputs(ChemicalHelper.get("raw", name, amount))
             .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
             .duration(200)
             .EUt(500)
@@ -26,7 +26,7 @@ ServerEvents.recipes(event => {
 
     event.recipes.gtceu.chemical_reactor("kubejs:undensify_oilsands")
         .itemInputs("kubejs:dense_oilsands_ore")
-        .itemOutputs('16x gtceu:raw_oilsands')
+        .itemOutputs('16x gtceu:endstone_oilsands_ore')
         .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
         .duration(200)
         .EUt(500)
@@ -37,19 +37,4 @@ ServerEvents.recipes(event => {
         .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
         .duration(200)
         .EUt(500)
-})
-
-ServerEvents.tags('item', event => {
-    // dense ore mod is gone, so we tag it
-    ores.forEach(ore => {
-        event.add("forge:ores/dense", `forge:ores/dense/${ore.name}`, `kubejs:dense_${ore.name}`);
-        // event.add('forge:dense_ores', `kubejs:dense_${ore}`);
-    })
-})
-
-ServerEvents.tags('block', event => {
-    // might as well
-    ores.forEach(ore => {
-        event.add("forge:ores/dense", `forge:ores/dense/${ore.name}`, `kubejs:dense_${ore.name}`);
-    })
 })
