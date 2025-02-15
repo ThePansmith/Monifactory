df4a806105330c80530ff12c47edf8fa370bf358
Xefyr0 <xefyr00@gmail.com>
2025-02-15 13:16:05 -0500

    Revert "RHF now can do ABS recipes"
    This reverts commit 1ba3d43de1f6e7955af55b78a012618c5e1bfabb.

--- kubejs/startup_scripts/modify_multiblocks.js
+++
@@ -1,8 +0,0 @@
-/**
- * Multiblock modification.
- * Only used for one thing at the moment.
- */
-
-GTCEuStartupEvents.registry('gtceu:machine', event => {
-    GCYMMachines.MEGA_BLAST_FURNACE.setRecipeTypes([GTRecipeTypes.BLAST_RECIPES, GTRecipeTypes.get("alloy_blast_smelter")])
-})
