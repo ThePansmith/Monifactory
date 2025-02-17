9615261c33b797af23d3e34f2ae6467ab37d42a5
Xefyr0 <xefyr00@gmail.com>
2025-02-16 21:27:09 -0500

    Better dense ore decompacting recipes

--- kubejs/server_scripts/gregtech/dense_ores.js
+++ kubejs/server_scripts/gregtech/dense_ores.js
@@ -3,37 +3,37 @@
  */
 
 const ores = [
-    ['redstone', 16],
-    ['diamond', 16],
-    ['emerald', 16],
-    ['gold', 16],
-    ['lapis', 16],
-    ['iron', 16],
-    ['coal', 16],
-    ['nether_quartz', 32],
-    ['copper', 16],
+    ['redstone', 4],
+    ['diamond', 4],
+    ['emerald', 4],
+    ['gold', 4],
+    ['lapis', 4],
+    ['iron', 4],
+    ['coal', 4],
+    ['nether_quartz', 2],
+    ['copper', 4],
 ]
 
 ServerEvents.recipes(event => {
     ores.forEach(([name, amount]) => {
         event.recipes.gtceu.chemical_reactor(`kubejs:undensify_${name}`)
-            .itemInputs(`kubejs:dense_${name}_ore`)
-            .itemOutputs(ChemicalHelper.get("raw", name, amount))
+            .itemInputs(`${amount}x kubejs:dense_${name}_ore`)
+            .itemOutputs(ChemicalHelper.get("raw", name, 64))
             .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
             .duration(200)
             .EUt(500)
     })
 
     event.recipes.gtceu.chemical_reactor("kubejs:undensify_oilsands")
-        .itemInputs("kubejs:dense_oilsands_ore")
-        .itemOutputs('16x gtceu:endstone_oilsands_ore')
+        .itemInputs("4x kubejs:dense_oilsands_ore")
+        .itemOutputs('64x gtceu:endstone_oilsands_ore')
         .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
         .duration(200)
         .EUt(500)
 
     event.recipes.gtceu.chemical_reactor("kubejs:undensify_magma")
-        .itemInputs("kubejs:dense_magma_block")
-        .itemOutputs('16x minecraft:magma_block')
+        .itemInputs("4x kubejs:dense_magma_block")
+        .itemOutputs('64x minecraft:magma_block')
         .inputFluids(Fluid.of("gtceu:nitric_acid", 1000))
         .duration(200)
         .EUt(500)
