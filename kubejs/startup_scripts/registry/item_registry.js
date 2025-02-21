c3b21471f70d99fb65d57c930be7a78201c3280a
Xefyr0 <xefyr00@gmail.com>
2025-02-21 05:19:13 -0500

    Limit stack size of miner kits to 16

--- kubejs/startup_scripts/registry/item_registry.js
+++ kubejs/startup_scripts/registry/item_registry.js
@@ -50,16 +50,16 @@ StartupEvents.registry("item", event => {
     event.create("quantum_flux").displayName("§dQuantum Flux")
 
     // Miner kits
-    event.create("gem_sensor").displayName("Gemstone Sensor")
-    event.create("basic_drilling_kit")
-    event.create("advanced_drilling_kit")
-    event.create("advanced_drilling_kit_ii").displayName("Advanced Drill Kit II")
-    event.create("blasting_kit")
-    event.create("microversal_alchemy_kit")
+    event.create("gem_sensor").maxStackSize(16).displayName("Gemstone Sensor")
+    event.create("basic_drilling_kit").maxStackSize(16)
+    event.create("advanced_drilling_kit").maxStackSize(16)
+    event.create("advanced_drilling_kit_ii").maxStackSize(16).displayName("Advanced Drill Kit II")
+    event.create("blasting_kit").maxStackSize(16)
+    event.create("microversal_alchemy_kit").maxStackSize(16)
 
     // T9+ Devices
-    event.create("gravity_well_generator")
-    event.create("universal_collapse_device")
+    event.create("gravity_well_generator").maxStackSize(16)
+    event.create("universal_collapse_device").maxStackSize(16)
 
     // Heavy platings
     event.create("quantum_fluxed_eternium_heavy_plating")
