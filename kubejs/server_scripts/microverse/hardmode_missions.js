58bea8ef468e14c3367c588358b3625094037f66
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2025-03-13 07:31:30 -0700

    Microminer Rebalancing (#1555)
    Mostly replacing dense versions of resources

--- kubejs/server_scripts/microverse/hardmode_missions.js
+++ kubejs/server_scripts/microverse/hardmode_missions.js
@@ -7,7 +7,7 @@ ServerEvents.recipes(event => {
             builder
                 .itemInputs(
                     "8x kubejs:quantum_flux",
-                    "minecraft:mossy_cobblestone"
+                    "minecraft:mossy_cobblestone",
                 )
                 .inputFluids("minecraft:lava 4000")
                 .itemOutputs(
@@ -15,7 +15,7 @@ ServerEvents.recipes(event => {
                     "64x minecraft:rotten_flesh",
                     "32x minecraft:rotten_flesh",
                     "32x minecraft:gunpowder",
-                    "16x minecraft:string"
+                    "16x minecraft:string",
                 )
         })
 
@@ -23,12 +23,12 @@ ServerEvents.recipes(event => {
             builder
                 .itemInputs(
                     "8x kubejs:quantum_flux",
-                    "minecraft:mud"
+                    "minecraft:mud",
                 )
                 .inputFluids("minecraft:lava 4000")
                 .itemOutputs(
                     "48x minecraft:slime_ball",
-                    "24x kubejs:guardian_scale"
+                    "24x kubejs:guardian_scale",
                 )
         })
 
@@ -36,14 +36,14 @@ ServerEvents.recipes(event => {
             builder
                 .itemInputs(
                     "8x kubejs:quantum_flux",
-                    "minecraft:redstone_block"
+                    "minecraft:redstone_block",
                 )
                 .inputFluids("minecraft:lava 8000")
                 .itemOutputs(
-                    "25x minecraft:blaze_rod",
-                    "25x thermal:blizz_rod",
-                    "25x thermal:blitz_rod",
-                    "25x thermal:basalz_rod"
+                    "16x minecraft:blaze_rod",
+                    "16x thermal:blizz_rod",
+                    "16x thermal:blitz_rod",
+                    "16x thermal:basalz_rod",
                 )
         })
 
@@ -51,13 +51,13 @@ ServerEvents.recipes(event => {
             builder
                 .itemInputs(
                     "8x kubejs:quantum_flux",
-                    "minecraft:netherrack"
+                    "minecraft:netherrack",
                 )
                 .inputFluids("minecraft:lava 8000")
                 .itemOutputs(
                     "24x minecraft:ghast_tear",
                     "48x kubejs:wither_bone", // drops of evil aren"t in the pack
-                    "32x minecraft:magma_cream"
+                    "32x minecraft:magma_cream",
                 )
         })
 
@@ -65,13 +65,14 @@ ServerEvents.recipes(event => {
             builder
                 .itemInputs(
                     "8x kubejs:quantum_flux",
-                    "minecraft:end_stone"
+                    "minecraft:end_stone",
                 )
                 .inputFluids("minecraft:lava 12000")
                 .itemOutputs(
-                    "12x enderio:enderman_head",
-                    "16x gtceu:ender_pearl_block",
-                    "32x minecraft:shulker_shell" // shulker pearls aren"t in the pack
+                    "16x enderio:enderman_head",
+                    "16x minecraft:ender_pearl",
+                    "16x minecraft:ender_pearl",
+                    "16x minecraft:shulker_shell", // shulker pearls aren"t in the pack
                 )
         })
 
@@ -80,7 +81,7 @@ ServerEvents.recipes(event => {
                 .itemInputs(
                     "8x kubejs:quantum_flux",
                     "32x kubejs:aerotheum_dust",
-                    "16x minecraft:ender_eye"
+                    "16x minecraft:ender_eye",
                 )
                 .itemOutputs(
                     "64x kubejs:dragon_lair_data",
@@ -95,12 +96,12 @@ ServerEvents.recipes(event => {
                 .itemInputs(
                     "8x kubejs:quantum_flux",
                     "32x kubejs:aerotheum_dust",
-                    "4x kubejs:dragon_lair_data"
+                    "4x kubejs:dragon_lair_data",
                 )
                 .itemOutputs(
                     "64x kubejs:ender_dragon_scale",
                     "32x kubejs:ender_dragon_scale",
-                    "64x minecraft:dragon_breath"
+                    "64x minecraft:dragon_breath",
                 )
         })
 
@@ -109,11 +110,13 @@ ServerEvents.recipes(event => {
                 .itemInputs(
                     "8x kubejs:quantum_flux",
                     "32x kubejs:aerotheum_dust",
-                    "64x kubejs:wither_bone"
+                    "64x kubejs:wither_bone",
                 )
                 .itemOutputs(
                     "64x kubejs:wither_realm_data",
-                    "16x gtceu:nether_star_block"
+                    "16x minecraft:nether_star",
+                    "16x minecraft:nether_star",
+                    "16x minecraft:nether_star",
                 )
         })
 
@@ -122,11 +125,11 @@ ServerEvents.recipes(event => {
                 .itemInputs(
                     "8x kubejs:quantum_flux",
                     "64x kubejs:aerotheum_dust",
-                    "16x minecraft:sculk_catalyst"
+                    "16x minecraft:sculk_catalyst",
                 )
                 .itemOutputs(
                     "64x kubejs:deep_dark_data",
-                    "16x kubejs:warden_horn"
+                    "16x kubejs:warden_horn",
                 )
         })
 
@@ -139,13 +142,13 @@ ServerEvents.recipes(event => {
                 .itemInputs("4x gtceu:neutron_reflector",)
                 .itemInputs(
                     "6x gtceu:ruridit_block",
-                    "4x kubejs:warden_horn"
+                    "4x kubejs:warden_horn",
                 )
                 .itemOutputs(
                     "64x gtceu:raw_darmstadtite",
                     "64x gtceu:raw_darmstadtite",
                     "64x gtceu:raw_darmstadtite",
-                    "64x gtceu:raw_darmstadtite"
+                    "64x gtceu:raw_darmstadtite",
                 )
         })
 
@@ -158,12 +161,12 @@ ServerEvents.recipes(event => {
                 .itemInputs("4x kubejs:gem_sensor")
                 .itemInputs(
                     "6x gtceu:naquadah_block",
-                    "4x kubejs:warden_horn"
+                    "4x kubejs:warden_horn",
                 )
                 .itemOutputs(
                     "64x gtceu:raw_dulysite",
                     "64x gtceu:raw_dulysite",
-                    "64x gtceu:raw_dulysite"
+                    "64x gtceu:raw_dulysite",
                 )
         })
 
@@ -176,11 +179,11 @@ ServerEvents.recipes(event => {
                 .itemInputs("4x nuclearcraft:plate_du")
                 .itemInputs(
                     "6x gtceu:uranium_block",
-                    "4x kubejs:warden_horn"
+                    "4x kubejs:warden_horn",
                 )
                 .itemOutputs(
                     "64x gtceu:raw_plutonium",
-                    "64x gtceu:raw_plutonium"
+                    "64x gtceu:raw_plutonium",
                 )
         })
 
@@ -196,7 +199,7 @@ ServerEvents.recipes(event => {
                     "64x gtceu:raw_electrotine",
                     "64x gtceu:raw_electrotine",
                     "64x gtceu:raw_electrotine",
-                    "64x gtceu:raw_electrotine"
+                    "64x gtceu:raw_electrotine",
                 )
         })
     }
