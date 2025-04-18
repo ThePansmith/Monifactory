61931b623d37f1129541fcc4489b9f9043cdb0d2
NegaNote <neganote43@gmail.com>
2025-04-18 01:39:53 -0400

    Update to GTMUtils 1.1.1 (#1699)
    * Update GTMUtils to 1.1.0
    
    This adds the PTERB.
    
    * Update gtmutils config with new defaults for monifactory
    
    * Move GTMUtils config to config-overrides, disable coolant for NM
    
    * Add Quantum Coolant recipe in KubeJS
    
    * ESLint tweaks
    
    * Update to hotfix 1.1.1
    
    * fix cfg
    
    ---------
    
    Co-authored-by: Pansmith <70342772+ThePansmith@users.noreply.github.com>

---
+++ kubejs/server_scripts/gregtech/gtmutils.js
@@ -0,0 +1,12 @@
+/** GTMUtils recipes */
+ServerEvents.recipes(event => {
+    if (doQuantumCoolant) {
+        event.recipes.gtceu.large_chemical_reactor("quantum_coolant")
+            .notConsumable("gtceu:uv_emitter")
+            .itemInputs("8x kubejs:quantum_flux", "8x kubejs:cryotheum_dust")
+            .inputFluids("gtceu:liquid_helium 1000")
+            .outputFluids("gtmutils:quantum_coolant 2000")
+            .duration(80)
+            .EUt(GTValues.VA[GTValues.LuV]);
+    }
+})
