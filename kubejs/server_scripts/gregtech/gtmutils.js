dc9640658f5ac3a6a97d41162a853716271175c6
NegaNote <neganote43@gmail.com>
2025-04-18 20:48:28 -0400

    Update Quantum Coolant recipe
    Change the quantum coolant recipe to use a neutron emitter as its catalyst and involve PCB coolant in its production.

--- kubejs/server_scripts/gregtech/gtmutils.js
+++ kubejs/server_scripts/gregtech/gtmutils.js
@@ -2,9 +2,9 @@
 ServerEvents.recipes(event => {
     if (doQuantumCoolant) {
         event.recipes.gtceu.large_chemical_reactor("quantum_coolant")
-            .notConsumable("gtceu:uv_emitter")
+            .notConsumable("kubejs:neutron_emitter")
             .itemInputs("8x kubejs:quantum_flux", "8x kubejs:cryotheum_dust")
-            .inputFluids("gtceu:liquid_helium 1000")
+            .inputFluids("gtceu:liquid_helium 1000", "gtceu:pcb_coolant 500")
             .outputFluids("gtmutils:quantum_coolant 2000")
             .duration(80)
             .EUt(GTValues.VA[GTValues.LuV]);
