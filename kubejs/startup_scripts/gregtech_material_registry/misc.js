1165a2a81188da0714ad1b755e2a781f892d3274
lilpaladin1 <lilpaladin_21@hotmail.com>
2025-05-04 15:00:34 -0500

    New Ardite Items

--- kubejs/startup_scripts/gregtech_material_registry/misc.js
+++ kubejs/startup_scripts/gregtech_material_registry/misc.js
@@ -459,6 +459,27 @@ GTCEuStartupEvents.registry("gtceu:material", event => {
         .color(0x997700)
 })
 
+// Netherite & Ardite Processing
+GTCEuStartupEvents.registry("gtceu:material", event => {
+    event.create("ardite_sand")
+        .dust()
+        .color(0xAD2F05)
+        .iconSet(GTMaterialIconSet.SAND)
+    event.create("ardite_salt")
+        .dust()
+        .color(0xE6734C)
+    event.create("nether_conduit")
+        .dust()
+        .color(0x664D4D)
+    event.create("ardite_slurry")
+        .fluid()
+        .color(0xAD2F05)
+    event.create("impure_molten_ardite")
+        .fluid()
+        .color(0xAD6639)
+        .fluidTemp(2500)
+})
+
 GTCEuStartupEvents.materialModification(event => {
     GTMaterials.RhodiumPlatedPalladium.setComponents("3x palladium", "1x rhodium", "2x lumium")
     GTMaterials.RhodiumPlatedPalladium.setFormula("Pd3Rh(SnFe)4(CuAg4)2", true)
