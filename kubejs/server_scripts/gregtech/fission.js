f5d0c40df04790d3c15bf3d0ba953f818291a8dd
iluha168 <44905605+iluha168@users.noreply.github.com>
2025-05-08 00:39:30 +0700

    KJS NCN GTMB :tr: (#1776)
    * add fission reactor multiblock
    
    * fix parallel hatch
    
    * template for fission recipes
    
    * fix infinite fission energy

---
+++ kubejs/server_scripts/gregtech/fission.js
@@ -0,0 +1,9 @@
+/**
+ * Fission reactor recipes
+ */
+
+ServerEvents.recipes(event => {
+    // TODO event.shaped("gtceu:fission_reactor")
+
+    // TODO event.recipes.gtceu.fission_reactor("gtceu:blah")
+})
