669714eae3ebff8e15fc4cdb4f5ba7e2f6f03ed1
NegaNote <neganote43@gmail.com>
2025-05-29 21:51:54 -0400

    add ORB running recipe

--- kubejs/server_scripts/gregtech/creative_multis.js
+++ kubejs/server_scripts/gregtech/creative_multis.js
@@ -4,6 +4,12 @@
 
 ServerEvents.recipes(event => {
     event.recipes.gtceu.omnidimensional_power_singularity("kubejs:superfuel_infinite_power")
-        .inputFluids("gtceu:naquadah_superfuel 10")
+        .inputFluids("gtceu:naquadah_superfuel 50")
+        .duration(20)
+
+    event.recipes.gtceu.omniscience_research_beacon("kubejs:infinite_research")
+        .inputFluids("gtceu:pcb_coolant 400")
+        .CWUt(256)
+        .EUt(GTValues.VA[GTValues.UHV])
         .duration(20)
 })
