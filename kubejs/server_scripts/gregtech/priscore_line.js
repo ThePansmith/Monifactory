1359a1a44a8ef59a1b9670ba0dd09854f11f0d56
SparkedTheorem <162088357+SparkedTheorem@users.noreply.github.com>
2025-06-17 17:18:26 -0700

    Fix usage of Omnic Forge recipe format (nonexistent now)

--- kubejs/server_scripts/gregtech/priscore_line.js
+++ kubejs/server_scripts/gregtech/priscore_line.js
@@ -28,7 +28,7 @@ ServerEvents.recipes(event => {
         .duration(200)
         .EUt(GTValues.VA[GTValues.UHV])
 
-    event.recipes.gtceu.omnic_forge("inert_prismatic_core")
+    event.recipes.gtceu.assembler("inert_prismatic_core")
         .itemInputs("kubejs:chromatically_inert_frame", "12x kubejs:chromatically_reactive_shard", "kubejs:heart_of_a_universe")
         .itemOutputs("kubejs:inert_prismatic_core")
         .duration(400)
