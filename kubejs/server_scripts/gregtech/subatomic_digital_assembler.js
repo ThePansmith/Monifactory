92a8fd3f5e10aac7452f5de34be0567196768033
Xefyr0 <xefyr00@gmail.com>
2025-06-19 00:26:34 -0400

    Add UEV hermetic casing, recipes for Quantum Chest and Tank X

--- kubejs/server_scripts/gregtech/subatomic_digital_assembler.js
+++ kubejs/server_scripts/gregtech/subatomic_digital_assembler.js
@@ -33,8 +33,6 @@ ServerEvents.recipes(event => {
 
     sda_print("creative_storage_data", 1, "functionalstorage:max_storage_upgrade", 128)
     sda_print("creative_storage_data", 2, "megacells:bulk_item_cell", 128)
-    sda_print("creative_storage_data", 3, "gtceu:uev_quantum_chest", 32)
-    sda_print("creative_storage_data", 4, "gtceu:uev_quantum_tank", 32)
     sda_print("creative_computation_data", 1, "gtceu:creative_data_access_hatch", 256)
     sda_print("creative_computation_data", 2, "gtceu:creative_computation_provider", 256)
     sda_print("creative_energy_data", 1, "enderio:creative_power", 256)
