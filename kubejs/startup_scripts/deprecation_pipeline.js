fe7146859269871d3a56203351004c9710814627
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2025-07-14 22:48:30 -0400

    Update Deprecation Pipeline to allow for deprecating blocks (#1937)

--- kubejs/startup_scripts/deprecation_pipeline.js
+++ kubejs/startup_scripts/deprecation_pipeline.js
@@ -4,13 +4,13 @@
  * in such a way that they can be replaced without players' items or fluids getting voided.
  *
  * To use this:
- * 1. Remove references to the item or fluid in every way possible, including
+ * 1. Delete references to the item or fluid in every way possible, including
  *    tags, recipes, tooltip, definition, lang, or other extra functionality.
- * 2. Use either of the utility functions below to specify the old and new IDs.
- *    If the item or fluid is created through KJS, delete the regular definition.
+ * 2. Use any of the utility functions below to specify the old and new IDs.
+ *    If the item, fluid, or block is created through KJS, delete the regular definition.
  *    The functions below create a replacement that is automatically substituted for the old one
  *    if you provide the correct ID.
- * 3. When fully removing the deprecated item or fluid, delete the call to the utility function to fully remove the old item.
+ * 3. When finally removing the deprecated item or fluid, delete the call to the utility function to fully remove the old item.
  *
  * TODO: add functionality for blocks
  */
@@ -28,6 +28,13 @@ global.deprecatedItems = {};
  * Used for tooltip & recipe generation for the deprecation pipeline.
  */
 global.deprecatedFluids = {};
+
+/**
+ * @type {Dictionary<ResourceLocation, ResourceLocation>}
+ * Dictionary of deprecated blocks and their replacements.
+ * Used for tooltip & recipe generation for the deprecation pipeline.
+ */
+global.deprecatedBlocks = {};
 
 /**
  * For a given item ID:
@@ -51,7 +58,7 @@ function deprecateItem(oldItemID, replacementItemID, oldItemName) {
 
 /**
  * For a given fluid ID:
- *   Creates an fluid with that ID if none exists
+ *   Creates a fluid with that ID if none exists
  *   Creates a recipe to convert the old fluid to the new fluid
  *   Applies a tooltip to the old fluid indicating that it's deprecated
  *
@@ -67,4 +74,24 @@ function deprecateFluid(oldFluidID, replacementFluidID, oldFluidName) {
             event.create(oldFluidID).displayName(`${oldFluidName} §c[DEPRECATED]`);
     })
     global.deprecatedFluids[oldFluidID] = replacementFluidID
+}
+
+/**
+ * For a given block ID:
+ *   Creates a block with that ID if none exists
+ *   Creates a recipe to convert an item with the old block ID to an item with the new block ID
+ *   Applies a tooltip to an item with the old block ID indicating that it's deprecated
+ *
+ * This allows the player to load their world and the generated block will replace the deprecated block,
+ * which can then be converted to the new, replacement block assuming the new item and block IDs are the same.
+ * @param {ResourceLocation} oldBlockID
+ * @param {ResourceLocation} replacementBlockID
+ * @param {String} oldBlockName
+ */
+function deprecateBlock(oldBlockID, replacementBlockID, oldBlockName) {
+    StartupEvents.registry("block", event => {
+        if(!Fluid.exists(oldBlockID))
+            event.create(oldBlockID).displayName(`${oldBlockName} §c[DEPRECATED]`);
+    })
+    global.deprecatedBlocks[oldBlockID] = replacementBlockID
 }
