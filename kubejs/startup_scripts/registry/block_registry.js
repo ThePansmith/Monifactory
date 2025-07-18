886f0fc819562aac42e348ab5da867b3526cf983
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2025-07-17 21:02:08 -0400

    Change P.R.I.S.M. base from Fusion Glass to LYSO:Ce Glass (#1930)
    * Change Fission Glass in P.R.I.S.M. to LYSO:Ce Glass
    
    * un-abbreviate LYSO:Ce
    
    * Fix glass recipe taking 4 dust instead of 1

--- kubejs/startup_scripts/registry/block_registry.js
+++ kubejs/startup_scripts/registry/block_registry.js
@@ -242,6 +242,15 @@ StartupEvents.registry("block", event => {
         .tagBlock("forge:mineable/wrench")
         .requiresTool(true);
 
+    event.create("lyso_ce_glass")
+        .displayName("Cerium-doped Lutetium Yttrium Oxyorthosilicate Glass")
+        .soundType("glass")
+        .renderType("cutout")
+        .resistance(6).hardness(5)
+        .tagBlock("mineable/pickaxe")
+        .textureAll("kubejs:block/prismac/lyso_ce_glass")
+        .defaultCutout()
+
     event.create("red_aligned_glass")
         .soundType("glass")
         .renderType("cutout")
