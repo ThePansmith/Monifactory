bad839cab7ae711a4176506ae55477eadeddd170
Xefyr0 <xefyr00@gmail.com>
2025-07-27 16:49:24 -0400

    Change IV sensor, emitter to use Rhodium instead of Iridium

--- kubejs/server_scripts/fixes_tweaks/delay_rmm.js
+++ kubejs/server_scripts/fixes_tweaks/delay_rmm.js
@@ -1,6 +1,6 @@
 /**
- * Changes the IV voltage coil to use Neptunium Palladium Aluminium,
- * to force players to build at least one(1) fission reactor for LEU-235 before entering IV
+ * Changes the IV voltage coil to use Neptunium Palladium Aluminium and the IV Sensor/Emitter to use Rhodium
+ * instead of Iridium to force players to build at least one(1) fission reactor for LEU-235 before entering IV
  * and simultaneously delay the necessity to process Rarest Metal Mixture to LuV.
  *
  * Also adds a mixer recipe for Neptunium Palladium Aluminium.
@@ -8,6 +8,7 @@
 
 ServerEvents.recipes(event => {
     event.replaceInput({ id: "gtceu:assembler/voltage_coil_iv" }, "gtceu:fine_iridium_wire", "gtceu:fine_neptunium_palladium_aluminium_wire")
+    event.replaceInput([{ id: "gtceu:shaped/sensor_iv" }, { id: "gtceu:shaped/emitter_iv" }], "gtceu:iridium_rod", "gtceu:rhodium_rod")
 
     event.recipes.gtceu.mixer("neptunium_palladium_aluminium")
         .itemInputs("gtceu:neptunium_dust", "5x gtceu:palladium_dust", "2x gtceu:aluminium_dust")
