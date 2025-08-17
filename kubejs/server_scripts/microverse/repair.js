1415df1bdfe02e64d6907206811cd98893643a9c
cewlboi <147581985+cewlboi@users.noreply.github.com>
2025-08-16 19:06:30 -0500

    Tweak half-tier MM repair recipes
    "repairing is cancer" - withers

--- kubejs/server_scripts/microverse/repair.js
+++ kubejs/server_scripts/microverse/repair.js
@@ -33,13 +33,7 @@ ServerEvents.recipes(event => {
             .itemInputs(`kubejs:microminer_t${minerTier}_damaged`)
             .itemOutputs(`kubejs:microminer_t${minerTier}`)
             .inputFluids(Fluid.of("gtceu:soldering_alloy", 36 * minerTierNumber))
-            .duration(20 * repairDurations[minerTier])
-
-        if(doublePlateList != undefined) {
-            doublePlateList.forEach((name, index) => {
-                builder.chancedInput(`2x gtceu:double_${name}_plate`, 9000 - 1000 * index, -1000 + 200 * index)
-            })
-        }
+            .duration(20 * repairDurations[minerTier])     
 
         return builder;
     }
@@ -70,14 +64,12 @@ ServerEvents.recipes(event => {
     // Combat miners always need repairs for engaging in combat
     repairing("2half", ["dark_steel"])
         .EUt(GTValues.VHA[GTValues.EV])
-        .chancedInput("minecraft:crossbow", 1600, -400)
-        .chancedInput("kubejs:hardened_thruster", 2000, -1000)
-        .chancedInput("kubejs:basic_micro_miner_guidance_system", 1500, -1500)
+        .chancedInput("2x gtceu:double_dark_steel_plate", 9500)
+        .chancedInput("minecraft:crossbow", 3000)
 
     repairing("4half", ["lumium", "hsse"])
         .EUt(GTValues.VHA[GTValues.IV])
-        .chancedInput("redstone_arsenal:flux_sword", 2400, -600)
-        .chancedInput("kubejs:energetic_thruster", 2000, -1000)
-        .chancedInput("kubejs:basic_micro_miner_guidance_system", 3000, -3000)
-        .chancedInput("kubejs:signalum_micro_miner_core", 1800, -600)
+        .chancedInput("2x gtceu:double_lumium_plate", 9000)
+        .chancedInput("2x gtceu:double_hsse_plate", 8000)
+        .chancedInput("redstone_arsenal:flux_sword", 2400)
 })
