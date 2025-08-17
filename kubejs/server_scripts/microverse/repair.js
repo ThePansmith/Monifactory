0de870701906b19903684e385034a889d5a34c43
WithersChat <76114055+WithersChat@users.noreply.github.com>
2025-08-17 20:18:06 +0200

    Update repair.js
    - removed double plate argument from "repairing" for clarity and greater control over balance.
    - Tweaked a few chances in preparation for XOR chance logic once it matches the way I was expecting it to work. Should use one ingredient stack per repair on average.

--- kubejs/server_scripts/microverse/repair.js
+++ kubejs/server_scripts/microverse/repair.js
@@ -22,7 +22,7 @@ ServerEvents.recipes(event => {
      * @param {string[]} doublePlateList List of the names of the double plate materials used to repair the hull.
      * @param {string[]} heavyPlatingList List of the names of the heavy plating materials used to repair the hull.
      */
-    function repairing(minerTier, doublePlateList) {
+    function repairing(minerTier) {
         let minerTierNumber;
 
         if(minerTier == "2half") minerTierNumber = 2.5;
@@ -38,23 +38,30 @@ ServerEvents.recipes(event => {
         return builder;
     }
 
-    repairing(6, ["enderium"])
+    // TODO: Rework those to exclude chance boosting too
+    repairing(6)
         .EUt(GTValues.VHA[GTValues.LuV])
+        .chancedInput("2x gtceu:double_enderium_plate", 9000, 0)
         .chancedInput("kubejs:supercharged_laser_array", 2800, -700)
         .chancedInput("kubejs:resonant_thruster", 3000, -1500)
         .chancedInput("kubejs:advanced_micro_miner_guidance_system", 3000, -1500)
         .chancedInput("kubejs:enderium_micro_miner_core", 1800, -600)
 
-    repairing(7, ["cryolobus", "naquadah"])
+    repairing(7)
         .EUt(GTValues.VA[GTValues.LuV])
+        .chancedInput("2x gtceu:double_cryolobus_plate", 9000, 0)
+        .chancedInput("2x gtceu:double_naquadah_plate", 8000, 0)
         .chancedInput("kubejs:supercharged_laser_array", 4000, -1000)
         .chancedInput("kubejs:warp_engine", 2100, -700)
         .chancedInput("kubejs:advanced_micro_miner_guidance_system", 3600, -1800)
         .chancedInput("kubejs:enderium_micro_miner_core", 1800, -600)
         .chancedInput(Item.of("kubejs:bathyal_energy_core", "{Damage:2000000}").weakNBT(), 1200, -400)
 
-    repairing(8, ["crystal_matrix", "rhodium_plated_palladium", "duranium"])
+    repairing(8)
         .EUt(GTValues.VA[GTValues.ZPM])
+        .chancedInput("2x gtceu:double_crystal_matrix_plate", 9000, 0)
+        .chancedInput("2x gtceu:double_rhodium_plated_palladium_plate", 9000, 0)
+        .chancedInput("2x gtceu:double_duranium_plate", 9000, 0)
         .chancedInput("kubejs:supercharged_laser_array", 4500, -900)
         .chancedInput("kubejs:warp_engine", 5100, -1700)
         .chancedInput("kubejs:advanced_micro_miner_guidance_system", 4000, -2000)
@@ -62,14 +69,14 @@ ServerEvents.recipes(event => {
         .chancedInput("kubejs:warp_controller", 2400, -600)
 
     // Combat miners always need repairs for engaging in combat
-    repairing("2half", ["dark_steel"])
+    repairing("2half")
         .EUt(GTValues.VHA[GTValues.EV])
-        .chancedInput("2x gtceu:double_dark_steel_plate", 9500, 0)
+        .chancedInput("2x gtceu:double_dark_steel_plate", 7000, 0)
         .chancedInput("minecraft:crossbow", 3000, 0)
 
-    repairing("4half", ["lumium", "hsse"])
+    repairing("4half")
         .EUt(GTValues.VHA[GTValues.IV])
         .chancedInput("2x gtceu:double_lumium_plate", 3500, 0)
         .chancedInput("2x gtceu:double_hsse_plate", 3300, 0)
-        .chancedInput("redstone_arsenal:flux_sword", 2400, 0)
+        .chancedInput("redstone_arsenal:flux_sword", 3200, 0)
 })
