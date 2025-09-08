8a8434c32fe8b9f3d7ce294ad0e68f10fc347713
Xefyr0 <xefyr00@gmail.com>
2025-09-08 15:38:06 -0400

    Reduce Eltz cost of Naquadah Superfuel

--- kubejs/server_scripts/gregtech/naquadah_fuel.js
+++ kubejs/server_scripts/gregtech/naquadah_fuel.js
@@ -115,7 +115,7 @@ ServerEvents.recipes(event => {
 
     event.recipes.gtceu.naquadah_refinery("naquadah_superfuel")
         .itemInputs("1x gtceu:infinity_dust", "20x kubejs:naquadah_fuel_mixture_dust")
-        .inputFluids("gtceu:hyperdegenerate_matter 200", "gtceu:naquadah_fuel 11000", "monilabs:eltz 576", "gtceu:quadium 400")
+        .inputFluids("gtceu:hyperdegenerate_matter 200", "gtceu:naquadah_fuel 11000", "monilabs:eltz 192", "gtceu:quadium 400")
         .outputFluids("gtceu:naquadah_superfuel 20000")
         .duration(2000)
         .EUt(GTValues.VA[GTValues.UHV])
