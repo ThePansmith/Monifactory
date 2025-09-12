cd425c8eb7b2ed0398f1581d23b36e51bbad041e
Jollahs <pcoolman13@gmail.com>
2025-09-11 18:57:13 -0600

    undo thingy

--- kubejs/server_scripts/gregtech/opticals.js
+++ kubejs/server_scripts/gregtech/opticals.js
@@ -194,7 +194,7 @@ ServerEvents.recipes(event => {
         .duration(800)
         .EUt(GTValues.VA[GTValues.UV])
         .inputFluids("gtceu:dielectric_mirror_solution 576")
-        .itemInputs("4x kubejs:carbon_mesh_reinforced_printed_circuit_board", "gtceu:luv_emitter", "8x gtceu:normal_optical_pipe", "12x gtceu:lumium_foil", "16x gtceu:duranium_bolt")
+        .itemInputs("4x kubejs:carbon_mesh_reinforced_printed_circuit_board", "gtceu:zpm_emitter", "8x gtceu:normal_optical_pipe", "12x gtceu:lumium_foil", "16x gtceu:duranium_bolt")
         .itemOutputs("4x kubejs:optical_processing_unit")
 
     // Non-PrismaC PSoC Steps
