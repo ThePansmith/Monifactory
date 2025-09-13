ThePansmith/Monifactory

commit b8cbedd0282d57296edd80ec72edbe5631a33d77

Main menu version display (#2101)
* add menu stuff

* auto set version from manifest

* adjust sizing

* linted

build/index.ts
@@ -104,6 +104,8 @@ async function packMod(group: string) {
             })
         }
 
+        fs.writeFileSync("dist/client/overrides/config/fancymenu/assets/pack-version.txt", readManifest().version)
+
         let hasZipCmd = false
         try {
             await Juke.exec("zip", ["--help"], { silent: true })

build/lib/manifest.ts
@@ -11,6 +11,7 @@ const zManifestFile = z.object({
 export type ManifestFileEntry = z.infer<typeof zManifestFile>
 
 const zManifest = z.object({
+    version: z.string().nonempty(),
     files: zManifestFile.array()
 })
 export type Manifest = z.infer<typeof zManifest>

config/fancymenu/custom_locals/monifactory/en_us.local
@@ -32,6 +32,8 @@ moni_titlescreen_difficulty_text_normal = Playing &9&lNormal&r Mode
 moni_titlescreen_difficulty_text_hard = Playing &9&lHard&r Mode
 moni_titlescreen_difficulty_text_expert = Playing &9&lExpert&r Mode
 
+moni_titlescreen_version = Version
+
 moni_bases_screen_back_to_menu_button = Back to Menu
 moni_bases_screen_back_to_menu_button_hover = &9Back to Menu
 

commit ccdd79c8612c210e0c5c2ace3815917d9132e8b7

Fixup! Main menu version display (#2111)
* Fix an issue introduced with #2101.

build/index.ts
@@ -78,11 +78,12 @@ async function packMod(group: string) {
     // fs.copyFileSync('pack-mode-switcher.bat', `dist/${group}/overrides/pack-mode-switcher.bat`)
     // fs.copyFileSync('pack-mode-switcher.sh', `dist/${group}/overrides/pack-mode-switcher.sh`)
 
+    const packSwitchPath = `dist/${group}${group !== "server" ? "/overrides" : ""}` // to account for server build not being in overrides
+
     try {
         if (process.platform === "win32") {
             // Switch build to NM if mode is unset
             if (!fs.existsSync("config/packmode.json")) {
-                const packSwitchPath = `dist/${group}${group !== "server" ? "/overrides" : ""}` // to account for server build not being in overrides
                 await Juke.exec("cmd.exe", ["/c", resolve("pack-mode-switcher.bat"), "-r", "-s", "n"], {
                     cwd: packSwitchPath
                 })
@@ -98,13 +99,12 @@ async function packMod(group: string) {
 
         // Switch build to NM if mode is unset
         if (!fs.existsSync("config/packmode.json")) {
-            const packSwitchPath = `dist/${group}${group !== "server" ? "/overrides" : ""}` // to account for server build not being in overrides
             await Juke.exec("./pack-mode-switcher.sh", ["-r", "-s", "n"], {
                 cwd: packSwitchPath
             })
         }
 
-        fs.writeFileSync("dist/client/overrides/config/fancymenu/assets/pack-version.txt", readManifest().version)
+        fs.writeFileSync(`${packSwitchPath}/config/fancymenu/assets/pack-version.txt`, readManifest().version)
 
         let hasZipCmd = false
         try {