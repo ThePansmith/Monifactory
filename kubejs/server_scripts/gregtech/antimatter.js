24a5f304e3457c4ea53b99e908e076d41000f96e
PlasmaticVoide <145876536+PlasmaticVoide@users.noreply.github.com>
2025-09-20 20:34:37 -0400

    Antimatter stuff downtier + protomatter buff (#2123)
    * make protomatter less ass
    
    * downtier anti stuff to UV
    
    * adjust powergen qb + reduce collider's Nt
    
    reduced manip's pipes but forgot about collider's

--- kubejs/server_scripts/gregtech/antimatter.js
+++ kubejs/server_scripts/gregtech/antimatter.js
@@ -13,7 +13,7 @@ ServerEvents.recipes(event => {
             .EUt(GTValues.VA[GTValues.ZPM])
 
         event.recipes.gtceu.antimatter_manipulator(`${type}_downgrade`)
-            .itemInputs("4x kubejs:protomatter")
+            .itemInputs("kubejs:protomatter")
             .inputFluids(`kubejs:refined_${type}_antimatter 36`)
             .outputFluids(`kubejs:${type}_antimatter 144`)
             .duration(200)
@@ -21,12 +21,12 @@ ServerEvents.recipes(event => {
     })
 
     event.recipes.gtceu.centrifuge("protomatter_separation")
-        .itemInputs("16x kubejs:protomatter")
+        .itemInputs("4x kubejs:protomatter")
         .outputFluids("kubejs:bosonic_antimatter 4", "kubejs:hadronic_antimatter 4", "kubejs:fermionic_antimatter 4")
         .duration(200)
-        .EUt(GTValues.VA[GTValues.ZPM])
+        .EUt(GTValues.VA[GTValues.UHV])
 
-    microverse_mission(event, 10, 3).forEach(builder => {
+    microverse_mission(event, 10, 3, 100).forEach(builder => {
         builder
             .inputFluids("gtceu:oxygen_plasma 16000")
             .itemInputs("kubejs:microversal_alchemy_kit")
@@ -45,23 +45,23 @@ ServerEvents.recipes(event => {
 
     // Controller Recipes
     event.recipes.gtceu.assembly_line("antimatter_manipulator")
-        .itemInputs("monilabs:bioalloy_fusion_casing", "#gtceu:circuits/uhv", "2x gtceu:uhv_field_generator", "2x monilabs:double_sculk_bioalloy_plate", "2x gtceu:uhv_emitter", "2x gtceu:uhv_electric_pump", "6x gtceu:neutronium_large_fluid_pipe", "32x gtceu:fine_ruthenium_trinium_americium_neutronate_wire")
-        .inputFluids("gtceu:advanced_soldering_alloy 2304", "gtceu:omnium 576", "gtceu:polyethyl_cyanoacrylate 1152")
+        .itemInputs("monilabs:dimensional_stabilization_netherite_casing", "#gtceu:circuits/uv", "2x gtceu:uv_field_generator", "2x gtceu:double_activated_netherite_plate", "2x gtceu:uv_emitter", "2x gtceu:uv_electric_pump", "3x gtceu:neutronium_normal_fluid_pipe", "32x gtceu:fine_enriched_naquadah_trinium_europium_duranide_wire")
+        .inputFluids("gtceu:advanced_soldering_alloy 2304", "gtceu:omnium 576", "gtceu:polybenzimidazole 1152")
         .itemOutputs("gtceu:antimatter_manipulator")
         .duration(200)
-        .EUt(GTValues.VA[GTValues.UHV])
+        .EUt(GTValues.VA[GTValues.UV])
         .stationResearch(b => b
-            .researchStack("monilabs:bioalloy_fusion_casing")
+            .researchStack("kubejs:protomatter")
             .CWUt(96, 28800)
             .EUt(GTValues.VA[GTValues.UV])
         )
 
     event.recipes.gtceu.assembly_line("antimatter_collider")
-        .itemInputs("6x monilabs:bioalloy_fusion_casing", "8x #gtceu:circuits/uhv", "16x monilabs:double_sculk_bioalloy_plate", "24x gtceu:hsse_frame", "12x solarflux:sp_custom_hadal", "4x gtceu:uhv_field_generator", "6x gtceu:uhv_emitter", "10x gtceu:uhv_electric_pump", "10x gtceu:superconducting_coil", "10x gtceu:neutronium_huge_fluid_pipe", "64x gtceu:fine_ruthenium_trinium_americium_neutronate_wire", "64x gtceu:fine_ruthenium_trinium_americium_neutronate_wire")
-        .inputFluids("gtceu:advanced_soldering_alloy 4608", "gtceu:omnium 1152", "gtceu:polyethyl_cyanoacrylate 2304")
+        .itemInputs("6x monilabs:dimensional_stabilization_netherite_casing", "8x #gtceu:circuits/uv", "16x gtceu:double_activated_netherite_plate", "24x gtceu:hsse_frame", "12x solarflux:sp_custom_hadal", "4x gtceu:uv_field_generator", "6x gtceu:uv_emitter", "10x gtceu:uv_electric_pump", "10x gtceu:superconducting_coil", "10x gtceu:neutronium_large_fluid_pipe", "64x gtceu:fine_enriched_naquadah_trinium_europium_duranide_wire", "64x gtceu:fine_enriched_naquadah_trinium_europium_duranide_wire")
+        .inputFluids("gtceu:advanced_soldering_alloy 4608", "gtceu:omnium 1152", "gtceu:polybenzimidazole 2304")
         .itemOutputs("gtceu:antimatter_collider")
         .duration(600)
-        .EUt(GTValues.VA[GTValues.UEV])
+        .EUt(GTValues.VA[GTValues.UHV])
         .stationResearch(b => b
             .researchStack("gtceu:implosion_collider")
             .CWUt(96, 28800)
