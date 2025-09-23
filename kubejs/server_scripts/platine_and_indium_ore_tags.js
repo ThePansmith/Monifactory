9e7171f5aac0651e7436efb173aa3c5c927c98f9
Phoenixvine <82596737+Phoenixvine32908@users.noreply.github.com>
2025-09-23 10:54:11 -0400

    Platine and Indium related ore tags.  (#2125)
    * Create platine_and_indium_ore_tags.js
    
    * Added "gtceu:purified_argentite_ore" tag to indium ores tag.

---
+++ kubejs/server_scripts/platine_and_indium_ore_tags.js
@@ -0,0 +1,8 @@
+/**
+Platline and Indium related ore tags for easier tag bus filtering.
+ */
+
+ServerEvents.tags("item", (event) => {
+    event.add("gtceu:purified_platline_ores", ["gtceu:purified_chalcopyrite_ore", "gtceu:purified_cooperite_ore", "gtceu:purified_pentlandite_ore", "gtceu:purified_bornite_ore", "gtceu:purified_chalcocite_ore", "gtceu:purified_tetrahedrite_ore"]);
+    event.add("gtceu:purified_indium_ores", ["gtceu:purified_sphalerite_ore", "gtceu:purified_galena_ore", "gtceu:purified_argentite_ore"]);
+});
