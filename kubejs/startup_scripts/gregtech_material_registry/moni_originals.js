75869b31a891fa8146c5b42d3d93c962c0def0ff
Xefyr0 <xefyr00@gmail.com>
2025-10-19 15:23:39 -0400

    Change HDD, Sculk Supercon recipes to match QB

--- kubejs/startup_scripts/gregtech_material_registry/moni_originals.js
+++ kubejs/startup_scripts/gregtech_material_registry/moni_originals.js
@@ -45,6 +45,11 @@ GTCEuStartupEvents.registry("gtceu:material", event => {
         .iconSet("crystal")
         .flags(GTMaterialFlags.NO_SMELTING, GTMaterialFlags.NO_SMASHING, GTMaterialFlags.NO_WORKING, GTMaterialFlags.DISABLE_DECOMPOSITION)
         .cableProperties(GTValues.V[GTValues.UHV], 8, 0, true)
+        .ignoredTagPrefixes([
+            TagPrefix.dust,
+            TagPrefix.dustSmall,
+            TagPrefix.dustTiny,
+        ])
 
 
     // Prismatic Crucible Materials
@@ -136,11 +141,15 @@ GTCEuStartupEvents.registry("gtceu:material", event => {
         .flags(GTMaterialFlags.GENERATE_FINE_WIRE, GTMaterialFlags.GENERATE_SPRING, GTMaterialFlags.GENERATE_SPRING_SMALL)
 
     event.create("hyperdegenerate_darconite")
-        .ingot()
-        .liquid(new GTFluidBuilder().state(GTFluidState.LIQUID).customStill()) // .liquid(new GTFluidBuilder().state(GTFluidState.MOLTEN).customStill()) (Not needed and doesn't work, but still kept around just in case. -Flare32)
         .color(0xffffff)
         .iconSet("hyperdegenerate_darconite")
         .components("3x darmstadtium", "4x cobalt", "2x nitrogen", "1x hyperdegenerate_matter")
         .cableProperties(GTValues.V[GTValues.UEV], 64, 0, true)
-        .flags(GTMaterialFlags.DISABLE_ALLOY_BLAST, GTMaterialFlags.NO_SMASHING, GTMaterialFlags.NO_WORKING, GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.GENERATE_FINE_WIRE)
+        .flags(GTMaterialFlags.DISABLE_ALLOY_BLAST, GTMaterialFlags.NO_SMELTING, GTMaterialFlags.NO_SMASHING, GTMaterialFlags.NO_WORKING, GTMaterialFlags.DISABLE_DECOMPOSITION, GTMaterialFlags.GENERATE_FINE_WIRE)
+        .ignoredTagPrefixes([
+            TagPrefix.dust,
+            TagPrefix.dustSmall,
+            TagPrefix.dustTiny,
+            TagPrefix.plate
+        ])
 })
