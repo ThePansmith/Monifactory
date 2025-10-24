921449185dc6a457c473f110f294f37f1f5564ad
iluha168 <illya.chernyshov888@gmail.com>
2025-10-24 22:55:20 +0700

    nuke old pack mode switchers (#2105)
    * nuke old pack mode switchers
    
    * chore: do not delete legacy switchers
    
    * fix: migrate to latest monilabs

---
+++ build/deps/pack_mode_switcher.ts
@@ -0,0 +1,50 @@
+import Juke from "juke-build"
+import { cacheFolderByManifestFileEntry, createDownloadModTarget } from "./mods.ts"
+import { readManifest } from "../lib/manifest.ts"
+import { join, resolve } from "path"
+import { readDatafileJSON } from "../lib/json_datafile.ts"
+
+const CORE_MOD_PROJECT_ID = 1266569
+
+const getCoreModManifestFileEntry = () => readManifest()
+    .files
+    .find(file => file.projectID === CORE_MOD_PROJECT_ID)
+
+export type TPackMode = "normal" | "hard" | "expert"
+
+export const getPackModeSwitchTarget = (
+    mode: TPackMode,
+    { cwd, dependsOn, onlyWhen }: {
+        cwd: string,
+        dependsOn?: Juke.Target[],
+        onlyWhen?: Juke.OnlyWhenFn,
+    }
+) => Juke.createTarget({
+    name: "switch-pack-mode-to-" + mode,
+
+    onlyWhen: async ctx => {
+        if (!await onlyWhen(ctx)) return false
+        try {
+            const { mode: existingMode } = readDatafileJSON(join(cwd, "config/packmode.json")) as { mode?: string }
+            return mode !== existingMode.toLowerCase()
+        } catch {
+            return true
+        }
+    },
+
+    // Needs the core mod jar
+    dependsOn: () => [createDownloadModTarget(getCoreModManifestFileEntry()), ...dependsOn],
+
+    executes: async () => {
+        const coreModFolder = cacheFolderByManifestFileEntry(getCoreModManifestFileEntry())
+        const [coreModJarPath] = Juke.glob(join(coreModFolder, "*.jar"))
+        if (!coreModJarPath) throw new Error("Core mod not found")
+
+        // Core mod performs pack switching when executed standalone
+        await Juke.exec(
+            "java",
+            ["-jar", resolve(coreModJarPath), mode, "--relative"],
+        )
+    }
+
+})
---
+++ build/lib/zip_mode_target.ts
@@ -0,0 +1,64 @@
+import fs from "fs"
+import Juke from "juke-build"
+import { resolve } from "path"
+import { getPackModeSwitchTarget } from "../deps/pack_mode_switcher.ts"
+import { readManifest } from "./manifest.ts"
+
+export const getZipModPackTarget = (
+    group: "server" | "client" | "dev",
+    dependsOn?: Juke.Target[],
+) => {
+    const packSwitchPath = `dist/${group}${group !== "server" ? "/overrides" : ""}`
+
+    return Juke.createTarget({
+
+        inputs: [
+            "manifest.json",
+            "dist/modlist.html",
+            "LICENSE.md",
+            `dist/${group}`,
+        ],
+
+        outputs: [
+            `dist/${group}.zip`,
+        ],
+
+        dependsOn: () => [
+        // Automatically switch to normal mode if no other mode was previously set
+            getPackModeSwitchTarget(
+                "normal",
+                {
+                    cwd: packSwitchPath,
+                    onlyWhen: () => !fs.existsSync("config/packmode.json"),
+                    dependsOn,
+                }
+            ),
+        ],
+
+        executes: async () => {
+            fs.copyFileSync("manifest.json", `dist/${group}/manifest.json`)
+            fs.copyFileSync("dist/modlist.html", `dist/${group}/modlist.html`)
+            fs.copyFileSync("LICENSE.md", `dist/${group}/LICENSE.md`)
+
+            fs.writeFileSync(`${packSwitchPath}/config/fancymenu/assets/pack-version.txt`, readManifest().version)
+
+            if (process.platform === "win32") {
+                await Juke.exec("powershell", [
+                    "Compress-Archive",
+                    `-Path "${resolve(`dist\\${group}\\*`)}"`,
+                    `-DestinationPath "${resolve(`dist\\${group}.zip`)}"`,
+                ])
+            } else {
+                Juke.logger.info("Running zip. This might take a while.")
+                await Juke.exec("zip", [
+                    "-qyr", // how queer
+                    `../${group}.zip`,  // file out
+                    ".", // include everything
+                ], {
+                    cwd: `dist/${group}`,
+                })
+            }
+        }
+
+    })
+}
