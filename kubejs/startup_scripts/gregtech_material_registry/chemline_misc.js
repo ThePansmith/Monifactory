5c0e1afe1729b569c1e93bfd357a7037350f9ecc
Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
2026-03-13 13:14:08 -0400

    Implement Wither Gas (#2517)
    * QB Tweaks and Fixes (#2451)
    
    * QB Tweaks and Fixes
    
    * go back to normal turbo chargers
    
    dont remember why i changed these in the first place
    
    * change other lang files
    
    * update gtm to 7.5.1 (and addons) (#2482)
    
    * update gtm to 7.5.1 (and addons)
    
    also includes autogenerated changes in some mod configs :thumbtel:
    
    * Revert "update gtm to 7.5.1 (and addons)"
    
    This reverts commit d5478caa660ced9a6081e63fcbe9831bdbd8229b.
    
    * updated pack from (new) manifest, now only jade config auto-updates
    
    * Change PR to update to 7.5.2
    
    ---------
    
    Co-authored-by: Xefyr0 <78383477+Xefyr0@users.noreply.github.com>
    
    * Update Forge installer version in README (#2487)
    
    * Band-aid fixing the broken platinum ore recipe (#2490)
    
    * Merge branch lang
    
    * Integral components tweaks (#2502)
    
    * Tweak Integral Components' progression
    
    * Update lang to match integral components' changes
    
    * Add PECA and PEEK variants to Electo-Optic Modulator (#2496)
    
    * update monilabs to 0.21.0 (#2508)
    
    * also adds recipes for the new advanced hatches
    
    * Add Project Red (#2480)
    
    * Add ProjectRed as a fully-fledged mod to the pack
    
    * Make the Illumination module optional
    
    * Merge branch lang
    
    * Update Monifactory_titlescreen_text.md
    
    I forgot to update this before :sobeline:
    
    This should reach the people who aren't in the discord.
    
    * update monilabs to 0.21.2 (#2515)
    
    * hotfix stuff for advanced sensor hatches
    
    * Copy over everything related to the PSoC diagrams (#2514)
    
    * Fix `"set-output" command is deprecated` in CI (#2509)
    
    * chore: upgrade versions of upstream actions
    
    * Merge branch lang
    
    ---------
    
    Co-authored-by: Release workflow <ThePansmith@users.noreply.github.com>
    
    * Implement Wither Gas
    
    * Tweak Alkylbenzene sulfonate recipe
    
    * Change recipe chain for more realism
    
    ---------
    
    Co-authored-by: PlasmaticVoide <145876536+PlasmaticVoide@users.noreply.github.com>
    Co-authored-by: NegaNote <neganote43@gmail.com>
    Co-authored-by: Fliptrack <58639522+Fliptrack@users.noreply.github.com>
    Co-authored-by: bjl32 <u.bjl32@outlook.com>
    Co-authored-by: Release workflow <ThePansmith@users.noreply.github.com>
    Co-authored-by: WithersChat <76114055+WithersChat@users.noreply.github.com>
    Co-authored-by: MEEPofFaith <54301439+MEEPofFaith@users.noreply.github.com>
    Co-authored-by: iluha168 <illya.chernyshov888@gmail.com>

--- kubejs/startup_scripts/gregtech_material_registry/chemline_misc.js
+++ kubejs/startup_scripts/gregtech_material_registry/chemline_misc.js
@@ -173,6 +173,25 @@ GTCEuStartupEvents.registry("gtceu:material", event => {
 
 })
 
+// Wither Gas
+GTCEuStartupEvents.registry("gtceu:material", event => {
+    event.create("tetrapropylene")
+        .fluid()
+        .color(0xbfa53f)
+
+    event.create("alkylbenzene")
+        .fluid()
+        .color(0xbcbf65)
+
+    event.create("overbased_magnesium_sulfonate")
+        .fluid()
+        .color(0x8b8c66)
+
+    event.create("wither_gas")
+        .gas()
+        .color(0x3c3d2d)
+})
+
 // Primal Mana ...Chemline?
 GTCEuStartupEvents.registry("gtceu:material", event => {
 
