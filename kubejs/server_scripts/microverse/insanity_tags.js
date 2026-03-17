4e6e5772ac6061b521567ffd82b66b2f4aaf2915
WithersChat <76114055+WithersChat@users.noreply.github.com>
2026-03-17 23:08:45 +0100

    Tag for Shards of Causality

---
+++ kubejs/server_scripts/microverse/insanity_tags.js
@@ -0,0 +1,13 @@
+/**
+ * Tag for degenerate Microverse stabilization challenge
+ */
+
+ServerEvents.tags("item", (event) => {
+    event.add("monilabs:degenerate_microverse_requestable", [
+        "gtceu:raw_emerald",
+        "gtceu:raw_dilithium",
+        "gtceu:gravi_star",
+        "minecraft:bone",
+        "kubejs:dragon_scale"
+    ])
+})
