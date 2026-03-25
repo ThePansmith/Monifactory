38c9f0e4f430b7af890d4eb6d60a8049d243ab7f
Apop T. Osis <janjaydenw730@gmail.com>
2026-03-24 21:20:31 -0400

    nomnomnomnom

---
+++ kubejs/server_scripts/nomnomnom.js
@@ -0,0 +1,22 @@
+if(doComplexProcessing) {
+    /**Nomnomnmonmonmonom */
+    ServerEvents.recipes(event => {
+        event.remove({ id: "gtceu:shaped/electric_blast_furnace"})
+
+        //Nomnomonmonmonmeowww
+        event.recipes.gtceu.shaped("gtceu:electric_blast_furnace", [
+            "FFF",
+            "MIM",
+            "TMT"
+        ], {
+            F: "gtceu:lv_electric_furnace",
+            M: '#gtceu:circuits/mv',
+            I: "gtceu:heatproof_machine_casing",
+            T: "gtceu:tin_single_cable"
+        }).id("gtceu:shaped/electric_blast_furnace")
+
+        // NOOOOM
+        event.remove({ type: "gtceu:primitive_blast_furnace", input: "minecraft:iron_ingot"})
+        event.remove({ type: "gtceu:primitive_blast_furnace", input: "#forge:storage_blocks/iron"})
+    })
+}
