c63477d668bbe9849dea2cb7c6a8a25c927a0464
Xefyr0 <xefyr00@gmail.com>
2026-06-21 08:24:17 -0400

    Re-add C&B as an optional compat

--- kubejs/startup_scripts/nukeLists/item.js
+++ kubejs/startup_scripts/nukeLists/item.js
@@ -46,6 +46,9 @@ global.itemNukeList = [
     // CBMultipart
     /cb_microblock:.*/,
 
+    // Chisels & Bits
+    "chiselsandbits:block_bit",
+
     // EnderIO
     "enderio:alloy_smelter",
     "enderio:clayed_glowstone",
